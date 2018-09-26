b5e455c82af3554eee782c1130f32cb7cf54fe92
Cody McCoy <cody@rmanet.com>
2018-09-26 08:57:26 -0700

    Changed the editors to include a row at the top that holds the new "SaveControl" which includes the undo, redo, and save button
    Copied from Perforce
     Change: 117930
    
    Former-commit-id: 80840062285dc8e4688997f08f13a74121f93df9

---
+++ Fda/Utilities/SaveControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Fda.Utilities
+{
+    /// <summary>
+    /// Interaction logic for SaveControl.xaml
+    /// </summary>
+    public partial class SaveControl : UserControl
+    {
+        //public static DependencyProperty ClickProperty = DependencyProperty.Register(nameof(Click), typeof(FdaViewModel.Utilities.NamedAction), typeof(BindableButton), new PropertyMetadata(ClickCallback));
+
+
+        //public object Click
+        //{
+        //    get { return (object)GetValue(ClickProperty); }
+        //    set { SetValue(ClickProperty, value); }
+        //}
+
+        public SaveControl()
+        {
+            InitializeComponent();
+        }
+
+        //public static void ClickCallback(object sender, DependencyPropertyChangedEventArgs args)
+        //{
+        //}
+    }
+}
