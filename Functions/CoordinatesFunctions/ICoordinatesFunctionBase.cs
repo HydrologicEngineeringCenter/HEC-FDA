8eea532d415b61c7d2c1f3a39816dda5319448ca
codymccoy <cody@rmanet.com>
2019-11-07 08:21:44 -0800

    Adding base interface
    Former-commit-id: bd0aa23380ebb591f07e5f0f40add3ceadb1c9f3

---
+++ Functions/CoordinatesFunctions/ICoordinatesFunctionBase.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions.CoordinatesFunctions
+{
+    public interface ICoordinatesFunctionBase
+    {
+
+    }
+}
