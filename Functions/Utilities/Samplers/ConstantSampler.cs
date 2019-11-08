54942942945dcafe1cd13d8edc06eda0fd7f2f4e
codymccoy <cody@rmanet.com>
2019-11-08 10:37:45 -0800

    Some missed files and added classes to wrap Statistics.IDistribution
    Former-commit-id: 2c61115ba6d63955b0c5f8a0b6316ca59228743f

---
+++ Functions/Utilities/Samplers/ConstantSampler.cs
@@ -0,0 +1,58 @@
+﻿using Functions.CoordinatesFunctions;
+using Functions.Ordinates;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Functions.Utilities
+{
+    public class ConstantSampler : ISampler
+    {
+        //I think the function will always be an OrdinateY function.
+        public bool CanSample(ICoordinatesFunction coordinatesFunction)
+        {
+            return (typeof(CoordinatesFunctionConstants).IsAssignableFrom(coordinatesFunction.GetType()));
+        }
+
+        public IFunction Sample(ICoordinatesFunction coordinatesFunction, double probability)
+        {
+
+            if (typeof(CoordinatesFunctionConstants).IsAssignableFrom(coordinatesFunction.GetType()))
+            {
+                return (CoordinatesFunctionConstants)coordinatesFunction;
+            }
+            throw new ArgumentException("Could not sample the coordinates function.");
+
+            //if (typeof(CoordinatesFunctionOrdinateYs).IsAssignableFrom(coordinatesFunction.GetType()))
+            //{
+            //    return ConvertOrdinateYsToConstant((CoordinatesFunctionOrdinateYs)coordinatesFunction);
+            //}
+
+            ////the can sample should have been checked before this method is called but i am being overly cautious
+            //if (CanSample(coordinatesFunction))
+            //{
+            //    //now that we know that it is <double, double> cast all coordinates into new list
+            //    List<ICoordinate<double, double>> constantCoords = new List<ICoordinate<double, double>>();
+            //    foreach (ICoordinate<XType, YType> coord in coordinatesFunction.Coordinates)
+            //    {
+            //        constantCoords.Add((ICoordinate<double, double>)coord);
+            //    }
+
+            //    //return a new constant function
+            //    return new CoordinatesFunctions.CoordinatesFunctionConstants(constantCoords);
+            //}
+            //throw new ArgumentException("Could not sample the coordinates function because the x and/or y values were of the wrong type.");
+        }
+
+        //private CoordinatesFunctionConstants ConvertOrdinateYsToConstant(CoordinatesFunctionOrdinateYs func)
+        //{
+        //    List<ICoordinate<Constant, Constant>> coords = new List<ICoordinate<Constant, Constant>>();
+        //    foreach(ICoordinate<Constant, IOrdinate> coord in func.Coordinates)
+        //    {
+        //        coords.Add(new Coordinates.CoordinateConstants(coord.X, new Constant(coord.Y.Value())));
+        //    }
+        //    return new CoordinatesFunctionConstants(coords, func.Interpolator);
+        //}
+
+    }
+}
---
+++ FunctionsTests/Coordinates/CoordinateVariableYTests.cs
@@ -0,0 +1,30 @@
+﻿using Functions.Coordinates;
+using Functions.Ordinates;
+using Statistics;
+using Statistics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+using Xunit;
+
+namespace FunctionsTests.Coordinates
+{
+    [ExcludeFromCodeCoverage]
+
+    public class CoordinateVariableYTests
+    {
+        /// <summary>
+        /// Tests the CoordinateConstants constructor with double values.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        [Fact]
+     
+        public void CoordinateConstants_GoodInput_Returns_CoordinateConstants()
+        {
+            CoordinateVariableY coord = new CoordinateVariableY(new Constant(1), new Distribution(new Normal(1,1)));
+            Assert.NotNull(coord);
+        }
+    }
+}
