839871269a72ebf5ab988b1998284867a132ac56
codymccoy <cody@rmanet.com>
2019-12-20 09:43:34 -0800

    Fixing error with Sample on the Distribution
    Former-commit-id: a283069080ef27552baa4224b96d08073d49b38f

--- Model/Condition/ComputePoint/ImpactAreaFunctions/FrequencyFunctions/DamageFrequency.cs
+++ Model/Condition/ComputePoint/ImpactAreaFunctions/FrequencyFunctions/DamageFrequency.cs
@@ -19,6 +19,8 @@ namespace Model.Condition.ComputePoint.ImpactAreaFunctions
         public bool IsValid { get; }
 
         public IEnumerable<IMessage> Errors { get; }
+
+        public IEnumerable<IMessage> Messages => throw new NotImplementedException();
         #endregion
 
 
