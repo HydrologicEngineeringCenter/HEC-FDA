7f19f69f65672aa3e86a750ef817bd331a51e4ad
codymccoy <cody@rmanet.com>
2020-02-05 11:15:38 -0800

    Adding occupancy type classes.
    Former-commit-id: 217f7ea38dabfa056ddbc848c4b4c0f51a3cd6fe

--- FdaViewModel/Inventory/OccupancyTypes/IOccupancyType.cs
+++ FdaViewModel/Inventory/OccupancyTypes/IOccupancyType.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FdaViewModel.Inventory.DamageCategory;
+using Functions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,23 @@ namespace FdaViewModel.Inventory.OccupancyTypes
 {
     interface IOccupancyType
     {
+        string Name { get; }
+
+        string Description { get; }
+
+        IDamageCategory DamageCategory { get; }
+
+        bool CalculateStructureDamage { get; }
+        bool CalcualateContentDamage { get; }
+        bool CalculateVehicleDamage { get; }
+        bool CalculateOtherDamage { get; }
+
+        ICoordinatesFunction StructureDepthDamageFunction { get; }
+        ICoordinatesFunction ContentDepthDamageFunction { get; }
+        ICoordinatesFunction VehicleDepthDamageFunction { get; }
+        ICoordinatesFunction OtherDepthDamageFunction { get; }
+
+
+
     }
 }
--- FdaViewModel/Inventory/OccupancyTypes/OccupancyType.cs
+++ FdaViewModel/Inventory/OccupancyTypes/OccupancyType.cs
@@ -6,7 +6,8 @@ using System.Threading.Tasks;
 
 namespace FdaViewModel.Inventory.OccupancyTypes
 {
-    class OccupancyType
+    internal class OccupancyType
     {
+
     }
 }
