cd4638878cf34b2e2f21bb9b14a83725f669165a
bbeam <Brennan.B.Beam@usace.army.mil>
2021-08-23 13:59:28 -0700

    remove old test OccupancyTypes fix #66

--- OccupancyTypesTests/UnitTest1.cs
+++ OccupancyTypesTests/UnitTest1.cs
@@ -7,13 +7,6 @@ namespace OccupancyTypesTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
-        {
-            string directoryName = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
-            string path = directoryName + "\\DamCatsAndOcctypes.txt";
-            OccupancyTypes.OccTypeGroupReader.ReadOccupancyTypeGroup(path);
-            int i = 0;
-        }
+
     }
 }
