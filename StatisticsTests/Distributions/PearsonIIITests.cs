914c09b429ab48327e3b51a6a22c7e64cabbf2b8
rnugent3 <richard.nugent.3@gmail.com>
2021-08-31 20:36:02 -0400

    added PearsonIII tests

--- StatisticsTests/Distributions/PearsonIIITests.cs
+++ StatisticsTests/Distributions/PearsonIIITests.cs
@@ -14,7 +14,14 @@ namespace StatisticsTests.Distributions
     public class PearsonIIITests
     {
         [Theory]
-        [InlineData(4434.739, 1681.290, 1.323, 1, .5, 4075.2)]
+        [InlineData(4434.739, 1681.290, 1.323, 1, .998, 11941.46931887674872996286)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .995, 10763.40989302623711409979)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .98, 8933.28373736705179908313)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .95, 7676.20361345045967027545)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .5, 4075.17124630542957675061)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .1, 2654.30189278692023435724)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .05, 2421.96631832806406237069)] //USGS-R SMWR
+        [InlineData(4434.739, 1681.290, 1.323, 1, .01, 2135.36487671968916401966)] //USGS-R SMWR
         public void PearsonIII_InverseCDF(double mean, double standardDeviation, double skew, int sampleSize, double p, double output)
         {
             var testObj = new Statistics.Distributions.PearsonIII(mean, standardDeviation, skew, sampleSize);
