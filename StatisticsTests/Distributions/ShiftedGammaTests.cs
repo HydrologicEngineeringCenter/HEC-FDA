04ccc8cc87d69531b263b6b6b8f3b2ad435c3c0a
rnugent3 <richard.nugent.3@gmail.com>
2021-08-31 20:46:03 -0400

    added ShiftedGamma Tests Fixes #73 Fixes #75 Fixes #76

--- StatisticsTests/Distributions/ShiftedGammaTests.cs
+++ StatisticsTests/Distributions/ShiftedGammaTests.cs
@@ -15,7 +15,14 @@ namespace StatisticsTests.Distributions
     public class ShiftedGammaTests
     {
         [Theory]
-        [InlineData(3.370, 614.880, 0, 0.5, 1868.6)]
+        [InlineData(3.370, 614.880, 1.2, 0.998, 6806.79323611756262835115)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.995, 6098.80793105717748403549)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.98, 4985.97495879006964969449)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.95, 4209.20458921972476673545)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.5, 1872.38437289608009450603)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.1, 821.04217088459745355067)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.05, 623.93076495493585298391)]//USGS-R SMWR
+        [InlineData(3.370, 614.880, 1.2, 0.01, 351.50666218164383280964)]//USGS-R SMWR
         public void ShiftedGamma_InverseCDF(double alpha, double beta, double shift, double p, double output)
         {
             var testObj = new Statistics.Distributions.ShiftedGamma(alpha, beta, shift);
