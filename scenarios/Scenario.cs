50ebdfca1dfd6593c37e8b8e0fd02699f2ae1ed5
HenryGeorgist <william.p.lehman@usace.army.mil>
2021-09-21 19:25:59 -0500

    Adding scenarios, fixes #4

---
+++ scenarios/Scenario.cs
@@ -0,0 +1,22 @@
+using System;
+using System.Collections;
+using impactarea;
+namespace scenarios
+{
+    public class Scenario
+    {
+        private Int64 _year;
+        private IList<ImpactArea> _impactAreas;
+        //probably need getters and setters
+        public Scenario(Int64 year, IList<ImpactArea> impactAreas){
+            _year = year;
+            _impactAreas = impactAreas;
+        }
+        public IList<double> Compute(Int64 seed, Int64 iterations){
+            //probably instantiate a rng to seed each impact area differently
+            foreach(ImpactArea ia in _impactAreas){
+                ia.Compute(seed, iterations);
+            }
+        }
+    }
+}
