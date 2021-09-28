980e7e88a8903662be2abdf99872caa2cbf79884
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-09-27 17:32:48 -0700

    Adding Paired data tests

--- fda-model-test/PairedDataTest.cs
+++ fda-model-test/PairedDataTest.cs
@@ -1,29 +1,44 @@
 using NUnit.Framework;
 using paireddata;
+using System.Collections.Generic;
 
 namespace fda_model_test
 {
     public class PairedDataTest
     {
-        public static double[] xs = { 0, 2000, 5000, 10000, 25000, 50000 };
-        private static double[] ys = { 556, 562, 565, 566, 570, 575 };
-        PairedData myPairedData = new PairedData(xs, ys);
+        static List<double> Flows = new List<double> { 0, 2000, 5000, 10000, 25000, 50000 };
+        static List<double> Stages = new List<double> { 556, 562, 565, 566, 570, 575 };
+        PairedData myRatingCurve = new PairedData(Flows, Stages);
 
-        [SetUp]
-        public void Setup()
-        {
- 
-        }
+        static List<double> Probabilities = new List<double> { .5, .2, .1, .05, .02, .01 };
+        PairedData myFlowFrequencyCurve = new PairedData(Probabilities, Flows);
+
 
         [Test]
-        public void TestGetXvals()
-        { 
-            Assert.AreEqual(xs,myPairedData.Xvals);
+        public void GetXvals()
+        {
+            Assert.AreEqual(Flows, myRatingCurve.Xvals);
         }
         [Test]
-        public void TestGetYvals()
+        public void GetYvals()
         {
-            Assert.AreEqual(ys, myPairedData.Yvals);
+            Assert.AreEqual(Stages, myRatingCurve.Yvals);
+        }
+        [Test]
+        public void FInterpolatesCorrect()
+        {
+            double expected = 565.5; //linear interp of ys when x=7500
+            double actual = myRatingCurve.f(7500);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void ComposeComposes()
+        {
+            IPairedData expected = new PairedData(Probabilities, Stages); //Probability-Stage
+            IPairedData actual = myRatingCurve.compose(myFlowFrequencyCurve);
+            Assert.AreEqual(expected.Xvals,actual.Xvals);
+            Assert.AreEqual(expected.Yvals, actual.Yvals);
+            
         }
     }
 }
