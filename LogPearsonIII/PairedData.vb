0cef816f82256dd65411b96631214d4a7ca6e5f3
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-09-28 13:53:10 -0700

    Delete LPIII VB Project

--- LogPearsonIII/PairedData.vb
+++
@@ -1,6 +0,0 @@
-﻿Public MustInherit Class PairedData
-    Public MustOverride Function Verify() As ErrorReport
-    Public MustOverride Sub ReadFromXelement(ele As XElement)
-    Public MustOverride Function WriteToXElement() As XElement
-    Public MustOverride Function Clone() As PairedData
-End Class
