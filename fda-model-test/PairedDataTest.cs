6ba7679689591fe2f534bb7a71af6f07128ca0e8
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-09-27 18:26:05 -0700

    add tests for f()

--- fda-model-test/PairedDataTest.cs
+++ fda-model-test/PairedDataTest.cs
@@ -6,7 +6,7 @@ namespace fda_model_test
 {
     public class PairedDataTest
     {
-        static double[] Flows = new double[] { 0, 2000, 5000, 10000, 25000, 50000 };
+        static double[] Flows = new double[] { 10, 2000, 5000, 10000, 25000, 50000 };
         static double[] Stages = new double[] { 556, 562, 565, 566, 570, 575 };
         PairedData myRatingCurve = new PairedData(Flows, Stages);
 
@@ -15,10 +15,31 @@ namespace fda_model_test
 
 
         [Test]
-        public void FInterpolatesCorrect()
+        public void FInterpolatesCorrectBetween()
         {
             double expected = 565.5; //linear interp of ys when x=7500
             double actual = myRatingCurve.f(7500);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void FReturnsMaxIfaAboveBounds()
+        {
+            double expected = 575;
+            double actual = myRatingCurve.f(100000);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void FReturnsMinIfBelowBounds()
+        {
+            double expected = 556;
+            double actual = myRatingCurve.f(0);
+            Assert.AreEqual(expected, actual);
+        }
+        [Test]
+        public void FReturnsExactIfExact()
+        {
+            double expected = 562;
+            double actual = myRatingCurve.f(2000);
             Assert.AreEqual(expected, actual);
         }
         [Test]
