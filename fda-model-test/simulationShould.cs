fff86542b5988583cb046b51db156df1a20952de
Will Lehman <william.p.lehman@usace.army.mil>
2021-10-19 09:35:43 -0700

    adding tests to verify different seeds create different sequences of EAD estimates

--- fda-model-test/simulationShould.cs
+++ fda-model-test/simulationShould.cs
@@ -45,6 +45,38 @@ namespace fda_model_test
             Assert.True(percentDiff < .01);
         }
         [Theory]
+        [InlineData(1234, 100, 138098)]
+        [InlineData(1234, 1, 336662)]
+        [InlineData(4321, 1, 150834)]
+        [InlineData(1111, 1, 78875)]
+        public void ComputeEAD_Iterations(int seed, int iterations, double expected)
+        {
+
+            Statistics.IDistribution flow_frequency = IDistributionFactory.FactoryUniform(0, 100000, 1000);
+            //create a stage distribution
+            IDistribution[] stages = new IDistribution[2];
+            for (int i = 0; i < 2; i++)
+            {
+                stages[i] = IDistributionFactory.FactoryUniform(0, 300000 * i, 10);
+            }
+            UncertainPairedData flow_stage = new UncertainPairedData(Flows, stages);
+            //create a damage distribution
+            IDistribution[] damages = new IDistribution[2];
+            for (int i = 0; i < 2; i++)
+            {
+                damages[i] = IDistributionFactory.FactoryUniform(0, 600000 * i, 10);
+            }
+            UncertainPairedData stage_damage = new UncertainPairedData(Stages, damages, "residential");
+            List<UncertainPairedData> upd = new List<UncertainPairedData>();
+            upd.Add(stage_damage);
+            Simulation s = new Simulation(flow_frequency, flow_stage, upd);
+            RandomProvider rp = new RandomProvider(seed);
+            metrics.IContainResults r = s.Compute(rp, iterations);
+            double difference = expected - r.MeanEAD("residential");
+            double percentDiff = difference / expected;
+            Assert.True(percentDiff < .01);
+        }
+        [Theory]
         [InlineData(0.0, 82500)]
         [InlineData(1.0, 115500)]
         public void ComputeEAD_withLevee(double failprobattop, double expected)
