69a359934c18af16a73e13fbffcd5291fc4f16a9
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2021-10-20 15:43:24 -0700

    Delete old statistics and utilities now in their own separate repos.

--- Statistics/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("StatisticsTests")]
-[assembly: InternalsVisibleTo("FunctionsTests")]
-[assembly: InternalsVisibleTo("ModelTests")]
