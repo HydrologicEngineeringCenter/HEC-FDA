679bbf3f19bd00bd295a2af29293940abcd10110
rnugent3 <richard.nugent.3@gmail.com>
2021-10-27 15:23:09 -0700

    revised assurance test to match new method signature

--- fda-model-test/ResultTest.cs
+++ fda-model-test/ResultTest.cs
@@ -11,31 +11,31 @@ namespace fda_model_test
     public class ResultTest
     {
         [Theory]
-        [InlineData(20000000)]
-        public void AssuranceOfAEP_Test(int n)
+        [InlineData(20000000, new double[8] { .5, .2, .1, .04, .02, .01, .004, .002 }, new double[8] { .5, .2, .1, .04, .02, .01, .004, .002 })]
+
+        public void AssuranceOfAEP_Test(int n, double[] exceedanceProb, double[] expected)
         {
-            double[] expected = new double[8]{.5, .2, .1, .04, .02, .01, .004, .002};
             Statistics.IDistribution uniform = new Statistics.Distributions.Uniform(0, 1);
             var rand = new Random(1234);
             Results results = new Results();
             double[] aeps = new double[n];
-            
+
             for (Int64 i = 0; i < n; i++)
             {
                 var randProb = rand.NextDouble();
                 aeps[i] = uniform.InverseCDF(randProb);
                 results.AddAEPEstimate(aeps[i]);
             }
-
-            double[] assuranceOfAEPs = new double[8];
-            assuranceOfAEPs = results.AssuranceOfAEP();
-            
-            for (int i=0; i<assuranceOfAEPs.Length; i++)
+            double[] assuranceOfAEP = new double[8];
+            for (int i = 0; i < exceedanceProb.Length; i++)
             {
-            double err = Math.Abs((expected[i] - assuranceOfAEPs[i]) / expected[i]);
-            double errTol = 0.5;
-            Assert.True(err < errTol);
+                assuranceOfAEP[i] = results.AssuranceOfAEP(exceedanceProb[i]);
+                double err = Math.Abs((expected[i] - assuranceOfAEP[i]) / expected[i]);
+                double errTol = 0.5;
+                Assert.True(err < errTol);
             }
         }
     }
-    }
+
+}
+    
