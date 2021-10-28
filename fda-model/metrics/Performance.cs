9f2adef08f55dcf1b425874f3283497e74ec1644
rnugent3 <richard.nugent.3@gmail.com>
2021-10-28 09:39:53 -0700

    added performance methods to Performance

--- fda-model/metrics/Performance.cs
+++ fda-model/metrics/Performance.cs
@@ -12,18 +12,78 @@ namespace metrics
 {
     private const double AEP_HISTOGRAM_BINWIDTH = .0001;
     private const double CNEP_HISTOGRAM_BINWIDTH = .01;
-    private string _thresholdType;
-    private double _thresholdValue;
+    private Threshold _threshold;
     private Histogram _aep = null;
     private Dictionary<string, Histogram> _ead;
     private Dictionary<double, Histogram> _cnep;
 
 
 
-    public Performance(string thresholdType, double thresholdValue)
+    public Performance(Threshold threshold)
     {
+            _threshold = threshold;
+            _aep = null; //is this necessary?
+            _cnep = new Dictionary<double, Histogram>();
 
     }
+        public void AddAEPEstimate(double aepEstimate)
+        {
+            double[] data = new double[1] { aepEstimate };
+            IData aep = IDataFactory.Factory(data);
+            if (_aep != null)
+            {
+                _aep.AddObservationToHistogram(aep);
+            }
+            else
+            {
+                var histo = new Histogram(aep, AEP_HISTOGRAM_BINWIDTH);
+                _aep = histo;
+            }
 
-}
+
+        }
+
+        public void AddStageForCNEP(double standardProbability, double stageForCNEP)
+        {
+            double[] data = new double[1] { stageForCNEP };
+            IData stage = IDataFactory.Factory(data);
+            if (_cnep.ContainsKey(standardProbability))
+            {
+                _cnep[standardProbability].AddObservationToHistogram(stage);
+            }
+            else
+            {
+                var histo = new Histogram(stage, CNEP_HISTOGRAM_BINWIDTH);
+                _cnep.Add(standardProbability, histo);
+            }
+        }
+
+        public double MeanAEP()
+        {
+            return _aep.Mean;
+        }
+
+        public double MedianAEP()
+        {
+            return _aep.Median;
+        }
+
+        public double AssuranceOfAEP(double exceedanceProbability)
+        {   //assurance of AEP is a non-exceedance probability
+            double assuranceOfAEP = _aep.CDF(exceedanceProbability);
+            return assuranceOfAEP;
+        }
+
+        public double ConditionalNonExceedanceProbability(double exceedanceProbability)
+        {
+            double conditionalNonExceedanceProbability = 1 - _cnep[exceedanceProbability].CDF(_threshold.ThresholdValue);
+            return conditionalNonExceedanceProbability;
+        }
+
+        public double LongTermExceedanceProbability(double years)
+        {
+            double ltep = 1 - Math.Pow((1 - MeanAEP()), years);
+            return ltep;
+        }
+    }
 }
