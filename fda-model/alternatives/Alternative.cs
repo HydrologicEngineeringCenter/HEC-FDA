be9392bde528526f5fc2ff05494af5c5a68b136d
rnugent3 <richard.nugent.3@gmail.com>
2021-10-28 19:00:17 -0700

    wrote PV routine but not sure how to interact with IContainResults

--- fda-model/alternatives/Alternative.cs
+++ fda-model/alternatives/Alternative.cs
@@ -7,23 +7,60 @@ namespace alternatives
     {
         private Scenario _currentYear;
         private Scenario _futureYear;
+        private Int64 _periodOfAnalysis;
+
         //probably need getters and setters
-        public Alternative(Scenario currentYear, Scenario futureYear){
+        public Alternative(Scenario currentYear, Scenario futureYear, Int64 periodOfAnalysis){
             _currentYear = currentYear;
             _futureYear = futureYear;
+            _periodOfAnalysis = periodOfAnalysis;
         }
         public IList<metrics.IContainResults> ComputeEEAD(interfaces.IProvideRandomNumbers rp, Int64 iterations, double discountRate){
             //probably instantiate a rng to seed each impact area differently
-            IList<metrics.IContainResults> currentEAD = _currentYear.Compute(rp,iterations);
-            IList<metrics.IContainResults> futureEAD = _futureYear.Compute(rp, iterations);
-            //discoiunt future ead
-            IList<metrics.IContainResults> discountedEAD = discount(futureEAD,discountRate, _futureYear.Year - _currentYear.Year);
-            //combine the future and current ead
-            return currentEAD;//not right, fix this.
+            IList<metrics.IContainResults> baseEAD = _currentYear.Compute(rp,iterations);
+            IList<metrics.IContainResults> mlfEAD = _futureYear.Compute(rp, iterations);
+            //I am not sure how to use the IContainResults
+            double[] interpolatedEADs = Interpolate(baseEAD, mlfEAD, _currentYear.Year, _futureYear.Year, _periodOfAnalysis);
+            double sumPresentValueEAD = PresentValueCompute(interpolatedEADs, discountRate);
+            double averageAnnualEquivalentDamage = IntoAverageAnnualEquivalentTerms(sumPresentValueEAD, _periodOfAnalysis, discountRate);
+          
+            return averageAnnualEquivalentDamage;
         }
-        private IList<metrics.IContainResults> discount(IList<metrics.IContainResults> eads, double discountRate, Int64 years){
-            //discount the eads
-            return eads;
+        private double IntoAverageAnnualEquivalentTerms(double sumPresentValueEAD, Int64 periodOfAnalysis, double discountRate)
+        {
+            double presentValueInterestFactorOfAnnuity = (1 - (1 / Math.Pow(1 + discountRate, periodOfAnalysis))) / discountRate;
+            double averageAnnualEquivalentDamage = sumPresentValueEAD / presentValueInterestFactorOfAnnuity;
+            return averageAnnualEquivalentDamage;
         }
+        private double PresentValueCompute(double[] interpolatedEADs, double discountRate)
+        {
+            Int64 periodOfAnalysis = interpolatedEADs.Length;
+            double[] presentValueInterestFactor = new double[periodOfAnalysis];
+            double sumPresentValueEAD = 0;
+            for (int i=1; i<periodOfAnalysis+1; i++)
+            {
+                presentValueInterestFactor[i] = 1 / Math.Pow(1 + discountRate, i);
+                sumPresentValueEAD += interpolatedEADs[i] * presentValueInterestFactor[i];
+            }
+            return sumPresentValueEAD;
+        }
+        private double[] Interpolate(double baseEAD, double mlfEAD, Int64 baseYear, Int64 mlfYear, Int64 periodOfAnalysis)
+        {
+            Int64 yearsBetweenBaseAndMLFInclusive = mlfYear - baseYear + 1;
+            Int64 yearsAfterMLF = periodOfAnalysis - mlfYear;
+            double[] interpolatedEADs = new double[periodOfAnalysis];
+            for (Int64 i =0; i<yearsBetweenBaseAndMLFInclusive; i++)
+            {
+                interpolatedEADs[i] = baseEAD + (i / yearsBetweenBaseAndMLFInclusive) * (baseEAD - mlfEAD);
+            }
+            for (Int64 i = yearsBetweenBaseAndMLFInclusive; i<periodOfAnalysis; i++)
+            {
+                interpolatedEADs[i] = mlfEAD;
+            }
+            return interpolatedEADs;
+        }
+
+
+
     }
 }
