4f7ca62ea52bb424e3a8985802dd9444ce6b42c2
rnugent3 <richard.nugent.3@gmail.com>
2021-10-28 17:07:12 -0700

    overloaded threshold constructor initiates project performance

--- fda-model/metrics/Threshold.cs
+++ fda-model/metrics/Threshold.cs
@@ -8,15 +8,50 @@ namespace metrics
 {
     public class Threshold
     {
-        public ThresholdEnum ThresholdType { get; }
-        public double ThresholdValue { get; }
-        public ProjectPerformance performance { get; set; }
+        public ThresholdEnum ThresholdType { get; set; }
+        public double ThresholdValue { get; set; }
+        public ProjectPerformance Performance { get; set; }
 
-        public Threshold(ThresholdEnum thresholdType, double thresholdValue)
+        public Threshold(bool isDefault, bool hasLevee, ead.Simulation simulation)
+        {
+            if (!isDefault)
+            {
+                throw new Exception("If not setting a default threshold, you must provide a threshold type and value");
+            }
+            else
+            {
+                this.SetDefault(hasLevee, simulation);
+                Performance = new ProjectPerformance(this.ThresholdType, this.ThresholdValue);
+            }
+            
+        }
+
+        public Threshold(ThresholdEnum thresholdType=0, double thresholdValue=0)
         {
             ThresholdType = thresholdType;
             ThresholdValue = thresholdValue;
-            performance = new ProjectPerformance(thresholdType,thresholdValue);
+            Performance = new ProjectPerformance(thresholdType, thresholdValue);
+        }
+
+        public void SetDefault(bool hasLevee, ead.Simulation simulation) //or simulation or ISimulation 
+        {
+            if (hasLevee)
+            {
+                ThresholdType = ThresholdEnum.ExteriorStage;
+                paireddata.IPairedData leveeCurve = new paireddata.PairedData(null, null); //instead, access levee from simulation
+                ThresholdValue = leveeCurve.Xvals.Max();
+            }
+            else
+            {
+                ThresholdType = ThresholdEnum.InteriorStage;
+                paireddata.IPairedData frequencyStage = new paireddata.PairedData(null, null); //instead access from simulation
+                paireddata.IPairedData stageDamage = new paireddata.PairedData(null, null); //instead access from simulation
+                paireddata.IPairedData damageFrequency = simulation.ComputeDamageFrequency(frequencyStage, stageDamage);
+                double percentOfDamage = 0.05;
+                double damageRecurrenceInterval = 0.01;
+                double significantDamage = percentOfDamage * damageFrequency.f_inverse(damageRecurrenceInterval);
+                ThresholdValue = stageDamage.f_inverse(significantDamage);
+            }
         }
     }
 }
--- fda-model/metrics/Thresholds.cs
+++ fda-model/metrics/Thresholds.cs
@@ -9,36 +9,12 @@ namespace metrics
     public class Thresholds
 {
         private Dictionary<int, Threshold> _thresholds;
-        internal Threshold _defaultThreshold;
 
-        public Thresholds(bool hasLevee, ead.Simulation simulation)
+        public Thresholds()
         {
             _thresholds = new Dictionary<int, Threshold>();
-            this.SetDefault(hasLevee, simulation);
-        }
-
-        public void SetDefault(bool hasLevee, ead.Simulation simulation) //or simulation or ISimulation 
-        {
-            if (hasLevee)
-            {
-                ThresholdEnum thresholdType = ThresholdEnum.ExteriorStage;
-                paireddata.IPairedData leveeCurve = new paireddata.PairedData(null,null); //instead, access levee from simulation
-                double thresholdValue = leveeCurve.Xvals.Max();
-                _defaultThreshold = new Threshold(thresholdType, thresholdValue);
-            } else
-            {
-                ThresholdEnum thresholdType = ThresholdEnum.InteriorStage;
-                paireddata.IPairedData frequencyStage = new paireddata.PairedData(null, null); //instead access from simulation
-                paireddata.IPairedData stageDamage = new paireddata.PairedData(null, null); //instead access from simulation
-                paireddata.IPairedData damageFrequency = simulation.ComputeDamageFrequency(frequencyStage,stageDamage);
-                double percentOfDamage = 0.05;
-                double damageRecurrenceInterval = 0.01;
-                double significantDamage = percentOfDamage * damageFrequency.f_inverse(damageRecurrenceInterval);
-                double thresholdValue = stageDamage.f_inverse(significantDamage);
-                _defaultThreshold = new Threshold(thresholdType, thresholdValue);
-            }
-            _thresholds.Add(0, _defaultThreshold);
-        }
+        
+        }      
 
         public void AddThreshold(int id, Threshold threshold)
         {
