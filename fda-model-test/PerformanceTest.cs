62535cc8db46c3e8250126f6a1d7d4c7890aa30b
rnugent3 <richard.nugent.3@gmail.com>
2021-10-28 17:42:03 -0700

    removed performance test until performance approach is settled

---
+++ fda-model-test/PerformanceTest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using metrics;
+
+namespace fda_model_test
+{
+    public class PerformanceTest
+    {
+        //TODO: write performance tests. removed previous test until peformance approach is settled. 
+    }
+
+}
+    
