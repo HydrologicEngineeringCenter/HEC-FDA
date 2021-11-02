0e534b22869bbd846b94526d66fb1ea9464aa558
codymccoy <cody@rmanet.com>
2021-11-02 10:18:00 -0700

    Started implementing task 2.

--- ViewModel/Alternatives/CreateNewAlternativeVM.cs
+++ ViewModel/Alternatives/CreateNewAlternativeVM.cs
@@ -8,57 +8,89 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ViewModel.Utilities;
 
 namespace ViewModel.Alternatives
 {
     public class CreateNewAlternativeVM : BaseEditorVM
     {
-        public List<ConditionWrapper> Conditions { get; set; }
+
+        public List<AlternativeRowItem> Rows { get; set; }
 
         public CreateNewAlternativeVM(List<InflowOutflowElement> conditions, EditorActionManager actionManager) : base(actionManager)
         {
-            //It is actually kind of difficult to get the list of selected items. The way i am going to do this is to create a simple
-            //object that holds a condition and a boolean, isSelected.
-            //so add to the constructor the list of conditions, then use them to create the wrapper class and then add those to the
-            //Conditions property.
-            List<ConditionWrapper> condWrappers = new List<ConditionWrapper>();
-            foreach(InflowOutflowElement cond in conditions)
+            List<IASElementSet> elems = StudyCache.GetChildElementsOfType<IASElementSet>();
+
+            List<AlternativeRowItem> rows = new List<AlternativeRowItem>();
+            foreach(IASElementSet elem in elems)
             {
-                ConditionWrapper condWrapper = new ConditionWrapper(cond);
-                condWrappers.Add(condWrapper);
+                AlternativeRowItem condWrapper = new AlternativeRowItem(elem);
+                rows.Add(condWrapper);
             }
-            Conditions = condWrappers;
+            Rows = rows;
         }
-        public override void AddValidationRules()
-        {
-            AddRule(nameof(Name), () => Name != null, "Name cannot be empty.");
-            AddRule(nameof(Name), () => Name != "", "Name cannot be empty.");
-        }
+        //public override void AddValidationRules()
+        //{
+        //    AddRule(nameof(Name), () => Name != null, "Name cannot be empty.");
+        //    AddRule(nameof(Name), () => Name != "", "Name cannot be empty.");
+        //}
         public override void Save()
         {
-            //Create a plan with the selected conditions
-            List<InflowOutflowElement> conditions = new List<InflowOutflowElement>();
-            foreach(ConditionWrapper cond in Conditions)
+            List<AlternativeRowItem> selectedRows = GetSelectedRows();
+            FdaValidationResult vr = IsValid(selectedRows);
+            if(vr.IsValid)
             {
-                if(cond.IsSelected)
-                {
-                    conditions.Add(cond.Condition);
-                }
+                AlternativeElement elemToSave = new AlternativeElement();
+                Saving.PersistenceFactory.GetExteriorInteriorManager
             }
+
         }
 
-        public class ConditionWrapper
+        private FdaValidationResult IsValid(List<AlternativeRowItem> selectedRows)
         {
-            public bool IsSelected { get; set; }
-            public string Name { get; set; }
-            public InflowOutflowElement Condition { get; set; }
-            public ConditionWrapper(InflowOutflowElement condition)
-            {
-                Condition = condition;
-                Name = Condition.Name;
-                IsSelected = false;
-            }
+
+            FdaValidationResult vr = new FdaValidationResult();
+
+            return vr;
         }
+
+        private List<IASElementSet> GetSelectedIASSets()
+        {
+            List<IASElementSet> selectedSets = new List<IASElementSet>();
+            foreach (AlternativeRowItem row in Rows)
+            {
+                if (row.IsSelected)
+                {
+                    selectedSets.Add(row.Element);
+                }
+            }
+            return selectedSets;
+        }
+        private List<AlternativeRowItem> GetSelectedRows()
+        {
+            List<AlternativeRowItem> selectedRows = new List<AlternativeRowItem>();
+            foreach(AlternativeRowItem row in Rows)
+            {
+                if(row.IsSelected)
+                {
+                    selectedRows.Add(row);
+                }
+            }
+            return selectedRows;
+        }
+
+        //public class ConditionWrapper
+        //{
+        //    public bool IsSelected { get; set; }
+        //    public string Name { get; set; }
+        //    public InflowOutflowElement Condition { get; set; }
+        //    public ConditionWrapper(InflowOutflowElement condition)
+        //    {
+        //        Condition = condition;
+        //        Name = Condition.Name;
+        //        IsSelected = false;
+        //    }
+        //}
 
 
     }
--- ViewModel/Study/FDACache.cs
+++ ViewModel/Study/FDACache.cs
@@ -70,9 +70,9 @@ namespace ViewModel.Study
         public event UpdateElementEventHandler StructureInventoryUpdated;
         public event UpdateElementEventHandler IASElementUpdated;
         public event UpdateElementEventHandler OccTypeElementUpdated;
-        public event AddElementEventHandler PlanAdded;
-        public event AddElementEventHandler PlanRemoved;
-        public event UpdateElementEventHandler PlanUpdated;
+        public event AddElementEventHandler AlternativeAdded;
+        public event AddElementEventHandler AlternativeRemoved;
+        public event UpdateElementEventHandler AlternativeUpdated;
 
 
         #region Properties
@@ -102,7 +102,8 @@ namespace ViewModel.Study
         public OccupancyTypesOwnerElement OccTypeParent { get; set; }
         public StructureInventoryOwnerElement StructureInventoryParent { get; set; }
         public IASOwnerElement IASParent { get; set; }
-        public AltervativeOwnerElement PlansParent { get; set; }
+        //public IASTreeOwnerElement IASTreeParent { get; set; }
+        public AltervativeOwnerElement AlternativeParent { get; set; }
         public AlternativeComparisonReportOwnerElement AlternativeComparisonReportParent { get; set; }
         #endregion
         #endregion
