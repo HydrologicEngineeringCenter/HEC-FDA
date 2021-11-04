cf7fff865cb181da4e210e3f58da23c93d217e62
codymccoy <cody@rmanet.com>
2021-11-04 08:28:16 -0700

    Finished up with the saving to the db and the loading back in. Live updating is working.

--- ViewModel/Alternatives/AlternativeElement.cs
+++ ViewModel/Alternatives/AlternativeElement.cs
@@ -101,7 +101,6 @@ namespace ViewModel.Alternatives
         }
         public void ViewResults(object arg1, EventArgs arg2)
         {
-
             //todo: Cody will do this in part 2 of task2.
         }
         public void EditAlternative(object arg1, EventArgs arg2)
@@ -141,8 +140,6 @@ namespace ViewModel.Alternatives
             }
             return altElement.ToString();
         }
-
-        
 
     }
 }
