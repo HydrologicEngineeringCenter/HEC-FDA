3e3058c76e8356511dadda7b0104476883875631
rnugent3 <richard.nugent.3@gmail.com>
2022-01-17 14:09:05 -0800

    added alternative comparison report

---
+++ fda-model/alternativeComparisonReport/AlternativeComparisonReport.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using alternatives;
+
+namespace alternativeComparisonReport
+{
+    class AlternativeComparisonReport
+{
+        private List<Alternative> _withProjectAlternatives;
+        private Alternative _withoutProjectAlternative;
+
+        public List<Alternative> WithProjectAlternatives
+        {
+            get
+            {
+                return _withProjectAlternatives;
+            } set
+            {
+                _withProjectAlternatives = value;
+            }
+        }
+        public Alternative WithoutProjectAlternative
+        {
+            get
+            {
+                return _withoutProjectAlternative;
+            }
+            set
+            {
+                _withoutProjectAlternative = value;
+            }
+        }
+
+        public AlternativeComparisonReport(Alternative withoutProject, List<Alternative> withProjecs)
+        {
+            _withoutProjectAlternative = withoutProject;
+            _withProjectAlternatives = withProjecs;
+        }
+}
+}
