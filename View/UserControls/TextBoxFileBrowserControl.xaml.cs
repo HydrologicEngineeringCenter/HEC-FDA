d29bd090d8e29a09eefb9ac230eb1fbf9e0400b3
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2022-01-19 10:22:00 -0800

    Adds user controls for file browser and folder browsers

---
+++ View/UserControls/TextBoxFileBrowserControl.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace View.UserControls
+{
+    public partial class TextBoxFileBrowserControl : UserControl
+    {
+        public static readonly DependencyProperty PathProperty = DependencyProperty.Register("Path", typeof(string), typeof(TextBoxFileBrowserControl), new FrameworkPropertyMetadata(null, new PropertyChangedCallback(PathChangedCallBack)));
+
+        public TextBoxFileBrowserControl()
+        {
+            InitializeComponent();
+            Filter = "XML files (*.xml) |*.xml";
+            FileExists = true;
+        }
+        public string Path
+        {
+            get { return Convert.ToString(GetValue(PathProperty)); }
+            set { SetValue(PathProperty, value);}
+        }
+        public bool FileExists { get; set; }
+        public bool IsOpenDialog { get; set; }
+        public string Filter { get; set; }
+        private static void PathChangedCallBack(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxFileBrowserControl owner = sender as TextBoxFileBrowserControl;
+            string p = e.NewValue.ToString();
+            owner.Path = p;
+            owner.TextBox.Text = p;
+        }
+
+        private void OpenBrowser(object sender, RoutedEventArgs e)
+        {
+            if (IsOpenDialog)
+            {
+                Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+                fileDialog.Filter = Filter;
+                fileDialog.Multiselect = false;
+                fileDialog.CheckFileExists = FileExists;
+                fileDialog.Title = "Open";
+                fileDialog.ShowDialog();
+                Path = fileDialog.FileName;
+
+            }
+            else
+            {
+                Microsoft.Win32.SaveFileDialog fileDialog = new Microsoft.Win32.SaveFileDialog();
+                fileDialog.Title = "Save As" ;
+                fileDialog.Filter = Filter;
+                fileDialog.ShowDialog();
+                Path = fileDialog.FileName;
+            }
+        }
+    }
+}
---
+++ View/UserControls/TextBoxFolderBrowserControl.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Microsoft.WindowsAPICodePack.Dialogs;
+using System.IO;
+
+namespace View.UserControls
+{
+    public partial class TextBoxFolderBrowserControl : UserControl
+    {
+        public static readonly DependencyProperty PathProperty = DependencyProperty.Register("Path", typeof(string), typeof(TextBoxFolderBrowserControl), new FrameworkPropertyMetadata(null, new PropertyChangedCallback(PathChangedCallBack)));
+
+        public TextBoxFolderBrowserControl()
+        {
+            InitializeComponent();
+        }
+        public string Path
+        {
+            get { return Convert.ToString(GetValue(PathProperty)); }
+            set { SetValue(PathProperty, value); }
+        }
+        public string InitialDirectory { get; set; } = Directory.GetCurrentDirectory();
+        private static void PathChangedCallBack(DependencyObject sender, DependencyPropertyChangedEventArgs e)
+        {
+            TextBoxFolderBrowserControl owner = sender as TextBoxFolderBrowserControl;
+            string p = e.NewValue.ToString();
+            owner.Path = p;
+            owner.TextBox.Text = p;
+        }
+        private void OpenBrowser(object sender, RoutedEventArgs e)
+        {
+            CommonOpenFileDialog dialog = new CommonOpenFileDialog();
+            dialog.InitialDirectory = InitialDirectory;
+            dialog.IsFolderPicker = true;
+            if (dialog.ShowDialog() == CommonFileDialogResult.Ok)
+            {
+                Path = dialog.FileName;
+            }
+        }
+    }
+}
