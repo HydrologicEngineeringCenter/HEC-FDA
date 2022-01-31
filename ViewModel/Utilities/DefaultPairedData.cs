8a42503ac17755ef0d0584797a6d2eb192e000e3
codymccoy <cody@rmanet.com>
2022-01-31 13:38:08 -0800

    Missed file

---
+++ ViewModel/Utilities/DefaultPairedData.cs
@@ -0,0 +1,28 @@
+﻿using paireddata;
+using Statistics.Distributions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ViewModel.Utilities
+{
+    public static class DefaultPairedData
+    {
+
+        public static UncertainPairedData CreateDefaultNormalUncertainPairedData(string xLabel, string yLabel, string name)
+        {
+            double[] xs = new double[10];
+            Normal[] ys = new Normal[10];
+            for (int i = 0; i < 10; i++)
+            {
+                xs[i] = i;
+                ys[i] = new Normal(i, 0);
+            }
+            UncertainPairedData curve = new UncertainPairedData(xs, ys, xLabel, yLabel, name, "", -1);
+            return curve;
+        }
+
+    }
+}
