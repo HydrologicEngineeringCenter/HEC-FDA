847a7225ab3f9b6a2666debcdde4bacd15265258
codymccoy <cody@rmanet.com>
2022-02-11 15:24:57 -0800

    missed file from last submit

---
+++ View/Utilities/BoolToUnidentifiableConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace View.Utilities
+{
+    public class BoolToUnidentifiableConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if ((bool)value == true)
+            {
+                return "Unidentifiable";
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
