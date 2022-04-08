1f032c8ea0f283360044b8bc8ebbe377b0bc496f
rnugent3 <richard.nugent.3@gmail.com>
2022-04-08 10:55:08 -0700

    started adding test data

--- fda-model-test/integrationtests/StudyDataGraphicalFlowFrequencyResultsTests.cs
+++ fda-model-test/integrationtests/StudyDataGraphicalFlowFrequencyResultsTests.cs
@@ -13,7 +13,22 @@ using Statistics.Distributions;
 
 namespace fda_model_test.integrationtests
 {
-    class StudyDataGraphicalFlowFrequencyResultsTests
+    /// <summary>
+    /// The example data in this test is based on Bear Creek Workshop 4 FDA study data
+    /// The data is based on PYSR Without, 2024, S Fork Bear, SF-8
+    /// see: https://drive.google.com/file/d/12WJL6ambACQLfqGUwbg7tv_wMxLn-a6t/view?usp=sharing
+    /// </summary>
+    public class StudyDataGraphicalFlowFrequencyResultsTests
     {
+        static int equivalentRecordLength = 48;
+        static double[] exceedanceProbabilities = new double[] { .999, .5, .2, .1, .04, .02, .01, .004, .002 };
+        static double[] flowFrequencyDischarges = new double[] { 900, 1500, 2120, 3140, 4210, 5070, 6240, 7050, 9680 };
+        static double[] ratingDischarges = new double[] { 0, 1500, 2120, 3140, 4210, 5070, 6240, 7050, 9680 };
+        static IDistribution[] stageDischargeStageDistributions = new IDistribution[]
+        {
+            new Normal(458,0);
+
+
+        }
     }
 }
