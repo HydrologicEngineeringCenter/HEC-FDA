f4dc08d07cacfcf351628d166c05b2e1a04c6daf
rnugent3 <richard.nugent.3@gmail.com>
2022-05-03 08:49:11 -0700

    ead results refactor created an expectedannualdamageresult object

---
+++ fda-model/metrics/ExpectedAnnualDamageResult.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Statistics.Histograms;
+using Statistics;
+
+namespace metrics
+{
+ public class ExpectedAnnualDamageResult
+{
+        #region Fields
+
+        private const double EAD_HISTOGRAM_BINWIDTH = 10;
+        private ThreadsafeInlineHistogram _eadHistogram;
+        private string _damageCategory;
+        private string _assetCategory;
+        #endregion
+
+        #region Properties
+        public ThreadsafeInlineHistogram EADHistogram
+        {
+            get
+            {
+                return _eadHistogram;
+            }
+        }
+        public string DamageCategory
+        {
+            get
+            {
+                return _damageCategory;
+            }
+        }
+        public string AssetCategory
+        {
+            get
+            {
+                return _assetCategory;
+            }
+        }
+        #endregion
+
+        #region Constructors
+        public ExpectedAnnualDamageResult(string damageCategory, string assetCategory, ConvergenceCriteria convergenceCriteria)
+        {
+            _damageCategory = damageCategory;
+            _assetCategory = assetCategory;
+            _eadHistogram = new ThreadsafeInlineHistogram(EAD_HISTOGRAM_BINWIDTH,convergenceCriteria);
+        }
+        #endregion
+
+        #region Methods
+        public void AddEADRealization(double ead, int iteration)
+        {
+            _eadHistogram.AddObservationToHistogram(ead, iteration);
+        }
+
+        public double MeanEAD()
+        {
+            return _eadHistogram.Mean;
+        }
+
+        public double EADExceededWithProbabilityQ(double exceedanceProbability)
+        {
+            double nonExceedanceProbability = 1 - exceedanceProbability;
+            double quartile = _eadHistogram.InverseCDF(nonExceedanceProbability);
+            return quartile;
+        }
+
+        public bool Equals(ExpectedAnnualDamageResult expectedAnnualDamageResult)
+        {
+                bool histogramsMatch = _eadHistogram.Equals(expectedAnnualDamageResult.EADHistogram);
+                if (!histogramsMatch)
+                {
+                    return false;
+                }
+            return true;
+        }
+        #endregion
+    }
+}
