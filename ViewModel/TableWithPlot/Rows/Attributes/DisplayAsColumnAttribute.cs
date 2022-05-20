67d01a423c6772d45817103434740daf137dd227
bbeam <Brennan.B.Beam@usace.army.mil>
2022-05-20 11:02:53 -0700

    remove the index from Disp as column attri

--- ViewModel/TableWithPlot/Rows/Attributes/DisplayAsColumnAttribute.cs
+++ ViewModel/TableWithPlot/Rows/Attributes/DisplayAsColumnAttribute.cs
@@ -6,19 +6,11 @@ namespace HEC.FDA.ViewModel.TableWithPlot.Rows.Attributes
     public class DisplayAsColumnAttribute : Attribute
     {
         private readonly string _displayName;
-        private readonly int _displayIndex;
         public string DisplayName { get { return _displayName; } }
 
-        public int DisplayIndex { get { return _displayIndex; } }
         public DisplayAsColumnAttribute(string displayName)
         {
             _displayName = displayName;
-            _displayIndex = -1;
-        }
-        public DisplayAsColumnAttribute(string displayName, int displayIndex )
-        {
-            _displayName = displayName;
-            _displayIndex = displayIndex;
         }
     }
 }
