b047f5548ba7ac019999ed09bd5bc9e2c4465b96
Jordan Lucas <jordan.lucas58@gmail.com>
2022-05-22 19:31:11 -0500

    Updated data that passes the stage-only integration test

--- fda-model-test/integrationtests/StudyDataGraphicalStageFrequencyResultsTests.cs
+++ fda-model-test/integrationtests/StudyDataGraphicalStageFrequencyResultsTests.cs
@@ -22,47 +22,63 @@ namespace fda_model_test.integrationtests
         static int impactareaid = 1;
         static int equivalentRecordLength = 25;
         static double[] exceedanceProbabilities = new double[] { .5, .2, .1, .04, .02, .01, .005, .002 };
-        static double[] stageFrequencyStages = new double[] { 375, 375.5, 376, 377, 379, 380.1, 381, 381.25 };
+        static double[] stageFrequencyStages = new double[] { 370, 373, 378, 381, 384, 385, 386, 387 };
        
-        static double[] stageDamageStages = new double[] { 374, 374.5, 375, 375.5, 376, 376.5, 377, 377.5, 378, 378.5, 379, 379.5, 380, 380.5, 381, 381.5, 382, 382.5, 383, 383.5, 384, 384.5, 385, 385.5, 386, 386.5, 387, 387.5, 388, 388.5, 389, 389.5, 390, 390.5, 391, 391.5};
+        static double[] stageDamageStages = new double[] { 369.5, 370, 370.5, 371, 371.5, 372, 372.5, 373, 373.5, 374, 374.5, 375, 375.5, 376, 376.5, 377, 377.5, 378, 378.5, 379, 379.5, 380, 380.5, 381, 381.5, 382, 382.5, 383, 383.5, 384, 384.5, 385, 385.5, 386, 386.5, 387, 387.5, 388, 388.5, 389, 389.5, 390, 390.5, 391, 391.5, 392, 392.5, 393, 393.5, 394, 394.5, 395 };
         static IDistribution[] stageDamageDamageDistributions = new IDistribution[]
         {
             new Normal(0,0),
-            new Normal(4.61,2.14),
-            new Normal(6.33, 2.52),
-            new Normal(8.45, 2.91),
-            new Normal(10.95, 3.34),
-            new Normal(13.76, 3.8),
-            new Normal(16.89, 4.3),
-            new Normal(20.29, 4.84),
-            new Normal(23.98, 5.43),
-            new Normal(27.89, 6.05),
-            new Normal(32.01, 6.7),
-            new Normal(36.28, 7.38),
-            new Normal(40.69, 8.1),
-            new Normal(45.21, 8.85),
-            new Normal(49.82, 9.61),
-            new Normal(54.48, 10.4),
-            new Normal(59.18, 11.21),
-            new Normal(63.9, 12.03),
-            new Normal(68.59, 12.84),
-            new Normal(73.21, 13.66),
-            new Normal(77.75, 14.46),
-            new Normal(82.19, 15.25),
-            new Normal(86.5, 16.02),
-            new Normal(90.64, 16.76),
-            new Normal(94.57, 17.46),
-            new Normal(98.3, 18.13),
-            new Normal(101.75, 18.75),
-            new Normal(104.94, 19.31),
-            new Normal(107.8, 19.83),
-            new Normal(110.36, 20.29),
-            new Normal(112.55, 20.68),
-            new Normal(114.36, 21.02),
-            new Normal(115.75, 21.27),
-            new Normal(116.73, 21.46),
-            new Normal(117.3, 21.57),
-            new Normal(117.55, 21.62)
+            new Normal(0,0),
+            new Normal(0,0),
+            new Normal(0,0),
+            new Normal(0,.03),
+            new Normal(.08,.37),
+            new Normal(.41,.84),
+            new Normal(.92,1.2),
+            new Normal(1.61,1.43),
+            new Normal(2.19,1.53),
+            new Normal(2.89,1.68),
+            new Normal(3.93,1.96),
+            new Normal(5.41,2.32),
+            new Normal(7.34,2.72),
+            new Normal(9.66,3.12),
+            new Normal(12.31,3.57),
+            new Normal(15.29,4.04),
+            new Normal(18.56,4.57),
+            new Normal(22.11,5.13),
+            new Normal(25.91,5.74),
+            new Normal(29.93,6.37),
+            new Normal(34.13,7.04),
+            new Normal(38.47,7.73),
+            new Normal(42.93,8.47),
+            new Normal(47.5,9.23),
+            new Normal(52.14,10.01),
+            new Normal(56.83,10.81),
+            new Normal(61.54,11.62),
+            new Normal(66.25,12.44),
+            new Normal(70.91,13.25),
+            new Normal(75.5,14.06),
+            new Normal(79.99,14.86),
+            new Normal(84.36,15.64),
+            new Normal(88.59,16.39),
+            new Normal(92.63,17.12),
+            new Normal(96.47,17.8),
+            new Normal(100.06,18.45),
+            new Normal(103.38,19.04),
+            new Normal(106.41,19.58),
+            new Normal(109.12,20.06),
+            new Normal(111.5,20.5),
+            new Normal(113.51,20.86),
+            new Normal(115.11,21.16),
+            new Normal(116.29,21.37),
+            new Normal(117.06,21.52),
+            new Normal(117.46,21.6),
+            new Normal(117.59,21.63),
+            new Normal(117.61,21.64),
+            new Normal(117.61,21.64),
+            new Normal(117.61,21.64),
+            new Normal(117.61,21.64),
+            new Normal(117.61,21.64)
         };
         static string assetcategory = "structure";
         static string xLabel = "x label";
@@ -73,7 +89,7 @@ namespace fda_model_test.integrationtests
         static CurveMetaData curveMetaData = new CurveMetaData(xLabel, yLabel, name, category, curveType);
         
         [Theory]
-        [InlineData(1234, 8.79)]
+        [InlineData(1234, 5.88)]
         public void ComputeMeanEADWithIterations_Test(int seed, double expected)
         {
             GraphicalUncertainPairedData stageFrequency = new GraphicalUncertainPairedData(exceedanceProbabilities, stageFrequencyStages, equivalentRecordLength, curveMetaData, usingStagesNotFlows: true);
