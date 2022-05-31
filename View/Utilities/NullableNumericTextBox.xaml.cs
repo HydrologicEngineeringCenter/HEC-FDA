d552bdeb3fb41e5095ed97bc248d6bcdcaf5fddb
Ryan Miles <ryanm@rmanet.com>
2022-05-31 11:05:05 -0700

    Adding .idea to .gitingore (was causing some issues for me). Updates to NullableNumericTextBox.

--- View/Utilities/NullableNumericTextBox.xaml.cs
+++ View/Utilities/NullableNumericTextBox.xaml.cs
@@ -8,86 +8,64 @@ namespace HEC.FDA.View.Utilities
     /// <summary>
     /// Interaction logic for NullableNumericTextBox.xaml
     /// </summary>
-    public partial class NullableNumericTextBox : UserControl
+    public partial class NullableNumericTextBox : TextBox
     {
-        public static readonly DependencyProperty DoubleValueProperty = DependencyProperty.Register(nameof(DoubleValue), typeof(double?), typeof(NullableNumericTextBox), new PropertyMetadata(DoubleValueChangedCallback));
+        public static readonly DependencyProperty DoubleValueProperty = DependencyProperty.Register(nameof(DoubleValue), typeof(double?), typeof(NullableNumericTextBox));
 
         public double? DoubleValue
         {
             get { return (double?)GetValue(DoubleValueProperty); }
-            set { SetValue(DoubleValueProperty, value); }
+            set
+            {
+                SetValue(DoubleValueProperty, value);
+                if (!_updating)
+                {
+                    UpdateText();
+                }
+            }
         }
+
+        private bool _updating;
 
         public NullableNumericTextBox()
         {
             InitializeComponent();
         }
 
-        private void TextBox_PreviewTextInput(object sender, TextCompositionEventArgs e)
+        private void UpdateText()
         {
-            
-                    e.Handled = true;
-            
+            //double?::ToString returns a string?, so we need to account for that.
+            Text = DoubleValue?.ToString() ?? "";
+        }
 
-            //Regex regex = new Regex("[^0-9.-]+");
-            //e.Handled = regex.IsMatch(e.Text);
-            if ( sender is TextBox txt)
+        private void MyPreviewTextInput(object sender, TextCompositionEventArgs e)
+        {
+            e.Handled = true;
+            string value = CombineText(e.Text);
+            if (double.TryParse(value, out double result))
             {
-                string value = combined_text(txt, e.Text);
-                if (double.TryParse(value, out double result))
-                {
-                    e.Handled = false;
-                    DoubleValue = result;
-                }
-                
+                e.Handled = false;
+                _updating = true;
+                DoubleValue = result;
+                _updating = false;
             }
-            //get datacontext and set the value?
         }
 
-        private static void DoubleValueChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        private string CombineText(string newText)
         {
-            NullableNumericTextBox owner = (NullableNumericTextBox)d;
-
-            owner.DoubleValue = (double)e.NewValue;
-            owner.txtBox.Text = owner.DoubleValue.ToString();
-            //if (double.TryParse(val, out double result))
-            //{
-            //    //e.Handled = false;
-            //    DoubleValue = result;
-            //}
-            //else
-            //{
-            //    DoubleValue = null;
-            //}
-
+            string text = Text;
+            return text.Substring(0, SelectionStart) + newText + text.Substring(SelectionStart + SelectionLength);
         }
 
-        private string combined_text(TextBox tb, string newText)
+        private void MyPreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
-            return tb.Text.Substring(0, tb.SelectionStart) + newText + tb.Text.Substring(tb.SelectionStart + tb.SelectionLength);
-        }
-
-        private void txtBox_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
-        {
-            //e.Handled = true;
-
-
-            //Regex regex = new Regex("[^0-9.-]+");
-            //e.Handled = regex.IsMatch(e.Text);
-            if (sender is TextBox txt)
+            if (double.TryParse(Text, out double result))
             {
-                //string value = combined_text(sender as TextBox, e.Text);
-
-                if (double.TryParse(txt.Text, out double result))
-                {
-                    //e.Handled = false;
-                    DoubleValue = result;
-                }
-                else
-                {
-                    DoubleValue = null;
-                }
-
+                DoubleValue = result;
+            }
+            else
+            {
+                DoubleValue = null;
             }
         }
     }
