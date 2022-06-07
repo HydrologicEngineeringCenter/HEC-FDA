a39dfe1536bb5421d79045b4e4de407878838454
Brennan Beam <BrennanBeam@gmail.com>
2022-06-07 15:29:46 -0700

    Add connection to RASMapperLib and supporting classes

---
+++ fda-model/hydraulics/HydraulicProfile.cs
@@ -0,0 +1,74 @@
+﻿using fda_model.hydraulics.enums;
+using RasMapperLib;
+using RasMapperLib.Mapping;
+
+namespace fda_hydro.hydraulics
+{
+    public class HydraulicProfile
+    {
+        public double Probability { get; set; }
+        public string FilePath { get; set; }
+        public string TerrainPath { get; set; }
+        public HydraulicDataSource dataSourceFormat { get; set; }
+        public string ProfileName { get; set; }
+
+        public HydraulicProfile(double probability, string filepath, string terrainFile, HydraulicDataSource dataSource)
+        {
+            Probability = probability;
+            FilePath = filepath;
+            TerrainPath = terrainFile;
+            dataSourceFormat = dataSource;
+        }
+        public float[] GetDepths(PointMs pts)
+        {
+            // Terrain is going to get sampled every time unnecessarily. This is an opportunity for refactor
+            //create feature layers for the standard inputs
+            TerrainLayer terrain = new TerrainLayer("Terrain", TerrainPath);
+            // get terrain elevations
+            float[] terrainElevs = terrain.ComputePointElevations(pts);
+            // Construct a result from the given filename.
+
+            if (dataSourceFormat == HydraulicDataSource.UnsteadyHDF || dataSourceFormat == HydraulicDataSource.SteadyHDF)
+            {
+                return GetDepthsFromHDF(pts, terrainElevs);
+            }
+            else
+            {
+                return GetDepthsFromGrid(pts, terrainElevs);
+            }
+        }
+
+        private float[] GetDepthsFromGrid(PointMs pts, float[] terrainElevs)
+        {
+            //TODO Sample off grids
+            return null;
+        }
+
+        private float[] GetDepthsFromHDF(PointMs pts, float[] terrainElevs)
+        {
+            var rasResult = new RASResults(FilePath);
+            var rasGeometry = rasResult.Geometry;
+            var rasWSMap = new RASResultsMap(rasResult, MapTypes.Depth);
+
+            // Sample the geometry for the given points loaded from the shapefile.
+            // If the geometry is the same for all of the results, we can actually reuse this object.
+            // (It's pretty fast to recompute though, so I wouldn't bother)
+            RASGeometryMapPoints mapPixels = rasGeometry.MapPixels(pts);
+            // This will produce -9999 for NoData values.
+            float[] depthVals = null;
+
+            int profileIndex;
+            if (dataSourceFormat == HydraulicDataSource.UnsteadyHDF)
+            {
+                profileIndex = RASResultsMap.MaxProfileIndex;
+            }
+            else
+            {
+                profileIndex = rasResult.ProfileIndex(ProfileName);
+            }
+            rasResult.ComputeSwitch(rasWSMap, mapPixels, profileIndex, terrainElevs, null, ref depthVals);
+            return depthVals;
+        }
+    }
+}
+
---
+++ fda-model/hydraulics/enums/HydraulicDataSource.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace fda_model.hydraulics.enums
+{
+    public enum HydraulicDataSource
+    {
+        UnsteadyHDF,
+        SteadyHDF,
+        DepthGrid,
+        WSEGrid
+
+    }
+}
