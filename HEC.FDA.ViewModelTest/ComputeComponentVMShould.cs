880c81577b904f430ca5ceeb71b8afeb1eb078c7
Brennan Beam <BrennanBeam@gmail.com>
2022-06-13 15:23:23 -0700

    Create test project and add tests for serializing graphical

---
+++ HEC.FDA.ViewModelTest/ComputeComponentVMShould.cs
@@ -0,0 +1,18 @@
+﻿using HEC.FDA.ViewModel.TableWithPlot;
+using Xunit;
+
+namespace fda_viewmodel_test
+{
+    public class ComputeComponentVMShould
+    {
+        [Fact]
+        public void ReadAndWriteXML()
+        {
+            ComputeComponentVM expected = new ComputeComponentVM("Brennan");
+            expected.SelectedItem = expected.Options[2];
+            var el = expected.ToXML();
+            ComputeComponentVM Actual = new ComputeComponentVM(el);
+            Assert.Equal(expected.SelectedItem.GetType(), Actual.SelectedItem.GetType());
+        }
+    }
+}
---
+++ HEC.FDA.ViewModelTest/TableWithPlotVMShould.cs
@@ -0,0 +1,23 @@
+﻿using HEC.FDA.ViewModel.FrequencyRelationships;
+using HEC.FDA.ViewModel.TableWithPlot;
+using System.Xml.Linq;
+using Xunit;
+
+namespace fda_viewmodel_test
+{
+    
+    public class TableWithPlotVMShould
+    {
+        [Fact]
+        public void SaveAndLoadGraphicalToXML()
+        {
+            TableWithPlotVM expected = new TableWithPlotVM(new GraphicalVM("X", "xlabel", "ylabel"));
+            ((GraphicalVM)(expected.ComputeComponentVM)).EquivalentRecordLength = 1;
+            XElement ele = expected.ToXML();
+            TableWithPlotVM actual =  new TableWithPlotVM(ele);
+            GraphicalVM exp = ((GraphicalVM)(expected.ComputeComponentVM));
+            GraphicalVM act = ((GraphicalVM) (actual.ComputeComponentVM));
+            Assert.Equal(exp.EquivalentRecordLength, act.EquivalentRecordLength);
+        }
+    }
+}
