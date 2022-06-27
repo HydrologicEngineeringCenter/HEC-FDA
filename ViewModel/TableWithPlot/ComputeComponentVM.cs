01f125f7b82f80998efeea585c4133aff9a3ea6b
Brennan Beam <BrennanBeam@gmail.com>
2022-06-27 13:36:26 -0700

    remove unnecessary comment

--- ViewModel/TableWithPlot/ComputeComponentVM.cs
+++ ViewModel/TableWithPlot/ComputeComponentVM.cs
@@ -206,7 +206,7 @@ namespace HEC.FDA.ViewModel.TableWithPlot
         {
             XElement ele = new XElement(this.GetType().Name);
             ele.SetAttributeValue("selectedItem", SelectedItem.Name);
-            ele.SetAttributeValue("Name", Name); //why is this null?
+            ele.SetAttributeValue("Name", Name);
             ele.SetAttributeValue("DeterministicOnly", _DeterministicOnly);
             ele.SetAttributeValue("Description", Description);
 
