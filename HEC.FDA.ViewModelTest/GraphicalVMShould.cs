3090fa4f0928e66aaf4d5af2d8a247aed1432c6a
Brennan Beam <BrennanBeam@gmail.com>
2022-06-28 16:32:53 -0700

    update test namespaces. add test for GraphicalVM

--- HEC.FDA.ViewModelTest/GraphicalVMShould.cs
+++ HEC.FDA.ViewModelTest/GraphicalVMShould.cs
@@ -1,7 +1,10 @@
 using HEC.FDA.ViewModel.FrequencyRelationships;
+using Importer;
 using Xunit;
+using HEC.FDA.ViewModel.Utilities;
+using HEC.FDA.ViewModel.TableWithPlot.Rows;
 
-namespace fda_viewmodel_test
+namespace HEC.FDA.ViewModelTest
 {
     public class GraphicalVMShould
     {
@@ -23,5 +26,38 @@ namespace fda_viewmodel_test
             Assert.Equal(expected.XLabel, Actual.XLabel);
             Assert.Equal(expected.YLabel, Actual.YLabel);
         }
+
+        public void LoadFromAnImportFile()
+        {
+            double[] stages = new double[] { 1, 2, 3 };
+            double[] probs = new double[] { .99, .5, .01 };
+            int ERL = 45;
+
+            ProbabilityFunction pf = new ProbabilityFunction();
+            pf.ProbabilityFunctionTypeId = ProbabilityFunction.FrequencyFunctionType.GRAPHICAL;
+            pf.ProbabilityDataTypeId = ProbabilityFunction.ProbabilityDataType.STAGE_FREQUENCY;
+            pf.NumberOfGraphicalPoints = stages.Length;
+            pf.Stage = stages;
+            pf.ExceedanceProbability = probs;
+            pf.EquivalentLengthOfRecord = ERL;
+
+            GraphicalVM expected = new GraphicalVM(StringConstants.GRAPHICAL_FREQUENCY, StringConstants.EXCEEDANCE_PROBABILITY, StringConstants.STAGE);
+            expected.Name = StringConstants.GRAPHICAL_FREQUENCY;
+            expected.EquivalentRecordLength = ERL;
+            expected.UseFlow = false;
+            expected.SelectedItem.Data.Clear();
+            expected.SelectedItem.Data.Add(new GraphicalRow(.99, 1));
+            expected.SelectedItem.Data.Add(new GraphicalRow(.5, 2));
+            expected.SelectedItem.Data.Add(new GraphicalRow(.01, 3));
+
+            GraphicalVM actual = new GraphicalVM(pf);
+
+            Assert.Equal(expected.Name, actual.Name);
+            Assert.Equal(expected.XLabel, actual.XLabel);
+            Assert.Equal(expected.YLabel, actual.YLabel);
+            Assert.Equal(expected.UseFlow, actual.UseFlow);
+            Assert.Equal(expected.UseStage, actual.UseStage);
+            Assert.Equal(expected.EquivalentRecordLength, actual.EquivalentRecordLength);
+        } 
     }
 }
