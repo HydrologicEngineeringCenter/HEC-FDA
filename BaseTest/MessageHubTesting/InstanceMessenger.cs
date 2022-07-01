4a0efe6a627b9f5af96f8906fe62abbde34a0708
Brennan Beam <BrennanBeam@gmail.com>
2022-07-01 12:43:46 -0700

    Add test project

---
+++ BaseTest/MessageHubTesting/InstanceMessenger.cs
@@ -0,0 +1,24 @@
+﻿using HEC.MVVMFramework.Base.Implementations;
+using HEC.MVVMFramework.Base.Events;
+
+
+namespace BaseTest.MessageHubTesting
+{
+    public class InstanceMessenger : HEC.MVVMFramework.Base.Interfaces.IReportMessage
+    {
+        private int count = 0; //debug
+        public event MessageReportedEventHandler MessageReport;
+
+        public void ReportMessage(object sender, MessageEventArgs e)
+        {
+            MessageReport?.Invoke(sender, e);
+        }
+
+        public void Poke()
+        {
+            count++;
+            int hash = this.GetHashCode();
+            ReportMessage(this, new MessageEventArgs(new Message("poked ya" + " " + count)));
+        }
+    }
+}
