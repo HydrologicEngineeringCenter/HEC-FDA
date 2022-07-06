12a4bfb2b6a66746883f7636b400aebd7c9669ba
Brennan Beam <BrennanBeam@gmail.com>
2022-07-06 15:24:10 -0700

    Set up View Test project

---
+++ ViewTest/View/CounterControl.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace ViewTest.View
+{
+    public partial class CounterControl : UserControl
+    {
+        public CounterControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
