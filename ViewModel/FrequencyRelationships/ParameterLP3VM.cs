463a4d461463dc3b4775d85297505fb92dba4cdc
Brennan Beam <BrennanBeam@gmail.com>
2022-07-11 13:46:42 -0700

    Add new frequency function VMs

---
+++ ViewModel/FrequencyRelationships/ParameterLP3VM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEC.FDA.ViewModel.FrequencyRelationships
+{
+    internal class ParameterLP3VM
+    {
+    }
+}
