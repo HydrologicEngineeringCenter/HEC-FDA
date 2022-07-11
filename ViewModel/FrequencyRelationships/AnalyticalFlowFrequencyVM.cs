1d6e123945ebd0b1b1fd6dd8559903bfc975dbb9
Brennan Beam <BrennanBeam@gmail.com>
2022-07-11 13:50:22 -0700

    rename

---
+++ ViewModel/FrequencyRelationships/AnalyticalFlowFrequencyVM.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEC.FDA.ViewModel.FrequencyRelationships
+{
+    internal class AnalyticalFlowFrequencyVM
+    {
+        private FitToFlowsLP3VM _myFitToFlowsLP3VM;
+
+        public FitToFlowsLP3VM MyFitToFlowsLP3VM
+        {
+            get { return _myFitToFlowsLP3VM; }
+            set { _myFitToFlowsLP3VM = value; }
+        }
+
+        private ParameterLP3VM _myParameterLP3VM;
+
+        public ParameterLP3VM MyParameterLP3VM
+        {
+            get { return _myParameterLP3VM; }
+            set { _myParameterLP3VM = value; }
+        }
+
+
+    }
+}
