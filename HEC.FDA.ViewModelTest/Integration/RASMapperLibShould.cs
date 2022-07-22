55f2559b890f5993c707214512613e55f7e5d0be
Brennan Beam <BrennanBeam@gmail.com>
2022-07-22 13:13:40 -0700

    add test for RASMapperLibIntegration

---
+++ HEC.FDA.ViewModelTest/Integration/RASMapperLibShould.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RasMapperLib.Mapping;
+using Xunit;
+
+namespace HEC.FDA.ViewModelTest.Integration
+{
+    public class RASMapperLibShould
+    {
+        [Fact]
+        public void ReturnListOfPlanNamesInHDF()
+        {
+            String pathToResultFile = "C:\\Users\\q0hecbbb\\Projects\\FDA\\HydraulicExamplesForCody\\SteadyFlowResultFile\\Muncie.p09.hdf";
+            var result = new RasMapperLib.RASResults(pathToResultFile);
+            var profileNames = result.ProfileNames;
+            Assert.True(profileNames.Length > 1);
+        }
+    }
+}
