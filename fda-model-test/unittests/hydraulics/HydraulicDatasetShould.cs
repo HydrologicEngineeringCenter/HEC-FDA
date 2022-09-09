7a95b00d4245a69b6c5e72942ef0fe28f5660554
Brennan Beam <BrennanBeam@gmail.com>
2022-09-09 15:21:43 -0700

    Test to ensure profiles are sorted descending probability

---
+++ fda-model-test/unittests/hydraulics/HydraulicDatasetShould.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using compute;
+using paireddata;
+using Statistics;
+using Statistics.Histograms;
+using metrics;
+using alternatives;
+using Statistics.Distributions;
+using scenarios;
+using System.Xml.Linq;
+using fda_model.hydraulics;
+using fda_model.hydraulics.enums;
+
+namespace fda_model_test.unittests.hydraulics
+{
+    [Trait("Category", "Unit")]
+    public class HydraulicDatasetShould
+    {
+        [Fact]
+        void SortHydrulicProfilesbyDescendingProbability()
+        {
+            List<HydraulicProfile> profiles = new List<HydraulicProfile>();
+            double[] probs = new double[] { 0.5, 0.99, 0.01 };
+            foreach(double prob in probs)
+            {
+                profiles.Add(new HydraulicProfile(prob,"","", HydraulicDataSource.UnsteadyHDF,null));
+            }
+            HydraulicDataset dataset = new HydraulicDataset(profiles);
+
+            double[] expected = new double[] { 0.99, 0.5, 0.01 };
+            double[] actual = new double[3];
+            int count = 0;
+            foreach(HydraulicProfile profile in dataset.HydraulicProfiles)
+            {
+                actual[count] = profile.Probability;
+                count++;
+            }
+            Assert.Equal(expected, actual);
+
+        }
+    }
+}
