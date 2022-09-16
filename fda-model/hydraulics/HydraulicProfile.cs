0912bb51766518839be125632f17410130e5fb34
Brennan Beam <BrennanBeam@gmail.com>
2022-09-16 14:54:05 -0700

    refactor to use WSE

--- fda-model/hydraulics/HydraulicProfile.cs
+++ fda-model/hydraulics/HydraulicProfile.cs
@@ -13,7 +13,7 @@ namespace fda_model.hydraulics
         public HydraulicDataSource DataSourceFormat { get; set; }
         public string ProfileName { get; set; }
 
-        public HydraulicProfile(double probability, string filepath, string terrainFile, HydraulicDataSource dataSource, string profileName)
+        public HydraulicProfile(double probability, string filepath,  HydraulicDataSource dataSource, string profileName, string terrainFile = null)
         {
             Probability = probability;
             FilePath = filepath;
@@ -21,41 +21,38 @@ namespace fda_model.hydraulics
             DataSourceFormat = dataSource;
             ProfileName = profileName;
         }
-        public float[] GetDepths(PointMs pts)
+        public float[] GetWSE(PointMs pts)
         {
             
             TerrainLayer terrain = new TerrainLayer("Terrain", TerrainPath);
             float[] terrainElevs = terrain.ComputePointElevations(pts);
-            // Construct a result from the given filename.
 
-            if (DataSourceFormat == HydraulicDataSource.UnsteadyHDF || DataSourceFormat == HydraulicDataSource.SteadyHDF)
+            if (DataSourceFormat == HydraulicDataSource.WSEGrid)
             {
-                return GetDepthsFromHDF(pts, terrainElevs);
+                return GetWSEFromGrids(pts, terrainElevs);
             }
             else
             {
-                return GetDepthsFromGrid(pts, terrainElevs);
+                return GetWSEFromHDF(pts, terrainElevs);
             }
         }
 
-        private float[] GetDepthsFromGrid(PointMs pts, float[] terrainElevs)
+        private float[] GetWSEFromGrids(PointMs pts, float[] terrainElevs)
         {
             //TODO Sample off grids
             return null;
         }
 
-        private float[] GetDepthsFromHDF(PointMs pts, float[] terrainElevs)
+        private float[] GetWSEFromHDF(PointMs pts, float[] terrainElevs)
         {
             var rasResult = new RASResults(FilePath);
             var rasGeometry = rasResult.Geometry;
             var rasWSMap = new RASResultsMap(rasResult, MapTypes.Elevation);
 
             // Sample the geometry for the given points loaded from the shapefile.
-            // If the geometry is the same for all of the results, we can actually reuse this object.
-            // (It's pretty fast to recompute though, so I wouldn't bother)
             RASGeometryMapPoints mapPixels = rasGeometry.MapPixels(pts);
-            // This will produce -9999 for NoData values.
-            float[] depthVals = null;
+
+            float[] WSE = null;
 
             int profileIndex;
             if (DataSourceFormat == HydraulicDataSource.UnsteadyHDF)
@@ -66,8 +63,9 @@ namespace fda_model.hydraulics
             {
                 profileIndex = rasResult.ProfileIndex(ProfileName);
             }
-            rasResult.ComputeSwitch(rasWSMap, mapPixels, profileIndex, terrainElevs, null, ref depthVals);
-            return depthVals;
+            // This will produce -9999 for NoData values.
+            rasResult.ComputeSwitch(rasWSMap, mapPixels, profileIndex, terrainElevs, null, ref WSE);
+            return WSE;
         }
 
 
