bc6295965514034fc53fbbeea7df7d34b60ebeb6
Brennan Beam <BrennanBeam@gmail.com>
2022-09-16 14:59:43 -0700

    refactoring tests

---
+++ fda-model-test/unittests/hydraulics/HydraulicProfileShould.cs
@@ -0,0 +1,24 @@
+﻿using Xunit;
+using fda_model.hydraulics;
+using fda_model.hydraulics.enums;
+using structures;
+
+namespace fda_model_test.unittests
+{
+    public class HydraulicProfileShould
+    {
+        private const string pathToNSIShapefile = @"..\..\..\Resources\MuncieNSI\MuncieNSI.shp";
+        private const string pathToIAShapefile = @"..\..\..\Resources\MuncieImpactAreas\ImpactAreas.shp";
+        private const string pathToResult = @"..\..\..\Resources\MuncieResult\Muncie.p04.hdf";
+        private const string pathToTerrain = @"..\..\..\Resources\MuncieTerrain\Terrain (1)_30ft_clip.hdf";
+
+        [Fact]
+        public void GetWSEFromHDF()
+        {
+            HydraulicProfile profile = new HydraulicProfile(.01, pathToResult, HydraulicDataSource.UnsteadyHDF, "Max", pathToTerrain);
+            Inventory inventory = new Inventory(pathToNSIShapefile, pathToIAShapefile);
+            float[] wses = profile.GetWSE(inventory.GetPointMs());
+            Assert.Equal(696, wses.Length);
+        }
+    }
+}
