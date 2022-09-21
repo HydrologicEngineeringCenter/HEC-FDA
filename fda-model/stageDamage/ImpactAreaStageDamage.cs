7378b1267f55ea84725c029545d50dc18066bce7
Brennan Beam <BrennanBeam@gmail.com>
2022-09-21 11:30:18 -0700

    Major reorg. No logical changes

--- fda-model/stageDamage/ImpactAreaStageDamage.cs
+++ fda-model/stageDamage/ImpactAreaStageDamage.cs
@@ -33,7 +33,15 @@ namespace stageDamage
         private List<OccupancyType> _occupancyTypes;
         private HydraulicDataset _hydraulicDataset;
 
+        private double _minStageForArea;
+        private double _maxStageForArea;
 
+        
+        private RandomProvider randomProvider;
+        private ConvergenceCriteria convergenceCriteria;
+
+        private int seed = 1234;
+        private int numIntermediateStagesToCompute = 15;
         #endregion
 
         #region Properties 
@@ -41,7 +49,8 @@ namespace stageDamage
         #endregion
         #region Constructor
         public ImpactAreaStageDamage(int impactAreaID, Inventory inventory, List<OccupancyType> occupancyTypes, HydraulicDataset hydraulicDataset,
-            ContinuousDistribution analyticalFlowFrequency = null, GraphicalUncertainPairedData graphicalFrequency = null, UncertainPairedData dischargeStage = null)
+            RandomProvider random, ConvergenceCriteria convergence, ContinuousDistribution analyticalFlowFrequency = null, GraphicalUncertainPairedData graphicalFrequency = null,
+            UncertainPairedData dischargeStage = null)
         {
             //TODO: Validate provided functions here
             _AnalyticalFlowFrequency = analyticalFlowFrequency;
@@ -51,84 +60,61 @@ namespace stageDamage
             _inventory = inventory;
             _hydraulicDataset = hydraulicDataset;
             _occupancyTypes = occupancyTypes;
+            randomProvider = random;
+            convergenceCriteria = convergence;
+            SetMinAndMaxStage();
         }
         #endregion
 
         #region Methods
-        //TODO: This compute produces the uncertain paired data
-        //That means that we need to have all hydraulic profiles available 
-        //The list of stages to me appears to mean that we won't 
-        public List<UncertainPairedData> Compute(RandomProvider randomProvider, ConvergenceCriteria convergenceCriteria)
+        private void SetMinAndMaxStage()
         {
-            int seed = 1234;
-
-            //the list of stages makes up the x values of the stage damage UPD
-            List<double> allStagesAtIndexLocation = new List<double>();
-            //the list of consequence distribution results will be paired with the list of stages to produce a list of UPD
-            List<UncertainPairedData> results = new List<UncertainPairedData>();
-            List <ConsequenceDistributionResults> consequenceDistributionResults = new List<ConsequenceDistributionResults>();
-
-            //Find the min stage and max stage for the impact area index location 
-            double minStage;
-            double maxStage;
-            //TODO we need a stage-frequency function to reference for the compute below. 
-
-            PairedData stageFrequency;
-
-            if(_AnalyticalFlowFrequency != null)
+            if (_AnalyticalFlowFrequency != null)
             {
                 if (_DischargeStage != null)
                 {
                     double minFLow = _AnalyticalFlowFrequency.InverseCDF(MIN_PROBABILITY);
                     IPairedData minStagesOnRating = _DischargeStage.SamplePairedData(MIN_PROBABILITY);
-                    minStage = minStagesOnRating.f(minFLow);
+                    _minStageForArea = minStagesOnRating.f(minFLow);
 
                     double maxFLow = _AnalyticalFlowFrequency.InverseCDF(MAX_PROBABILITY);
                     IPairedData maxStagesOnRating = _DischargeStage.SamplePairedData(MAX_PROBABILITY);
-                    maxStage = maxStagesOnRating.f(maxFLow);
-
-                    Tuple<double[],double[]>  flowFreqAsTuple = _AnalyticalFlowFrequency.ToCoordinates();
-                    PairedData flowFrequencyPairedData = new PairedData(flowFreqAsTuple.Item1, flowFreqAsTuple.Item2);
-                    stageFrequency = _DischargeStage.SamplePairedData(0.5).compose(flowFrequencyPairedData) as PairedData;
+                    _maxStageForArea = maxStagesOnRating.f(maxFLow);
                 }
                 else
                 {
                     string message = "A stage-discharge function must accompany a flow-frequency function but no such function was found. Stage-damage compute aborted" + Environment.NewLine;
                     ErrorMessage errorMessage = new ErrorMessage(message, ErrorLevel.Fatal);
                     ReportMessage(this, new MessageEventArgs(errorMessage));
-                    return results;
                 }
-            } else if (_GraphicalFrequency != null)
+            }
+            else if (_GraphicalFrequency != null)
             {
-                if(_GraphicalFrequency.UsesStagesNotFlows)
+                if (_GraphicalFrequency.UsesStagesNotFlows)
                 {
                     IPairedData minStages = _GraphicalFrequency.SamplePairedData(MIN_PROBABILITY);
-                    minStage = minStages.Yvals[0];
+                    _minStageForArea = minStages.Yvals[0];
                     IPairedData maxStages = _GraphicalFrequency.SamplePairedData(MAX_PROBABILITY);
-                    maxStage = maxStages.Yvals[maxStages.Yvals.Length - 1];
-                    stageFrequency = _GraphicalFrequency.SamplePairedData(0.5) as PairedData;
-                } else
+                    _maxStageForArea = maxStages.Yvals[maxStages.Yvals.Length - 1];
+                }
+                else
                 {
                     if (_DischargeStage != null)
                     {
-                        PairedData flowFrequencyPairedData = _GraphicalFrequency.SamplePairedData(0.5) as PairedData;
-                        stageFrequency = _DischargeStage.SamplePairedData(0.5).compose(flowFrequencyPairedData) as PairedData;
-
                         IPairedData minFlows = _GraphicalFrequency.SamplePairedData(MIN_PROBABILITY);
                         double minFlow = minFlows.Yvals[0];
                         IPairedData minStages = _DischargeStage.SamplePairedData(MIN_PROBABILITY);
-                        minStage = minStages.f(minFlow);
+                        _minStageForArea = minStages.f(minFlow);
                         IPairedData maxFlows = _GraphicalFrequency.SamplePairedData(MAX_PROBABILITY);
                         double maxFlow = maxFlows.Yvals[maxFlows.Yvals.Length - 1];
                         IPairedData maxStages = _DischargeStage.SamplePairedData(MAX_PROBABILITY);
-                        maxStage = maxStages.f(maxFlow);
+                        _maxStageForArea = maxStages.f(maxFlow);
                     }
                     else
                     {
                         string message = "A stage-discharge function must accompany a flow-frequency function but no such function was found. Stage-damage compute aborted" + Environment.NewLine;
                         ErrorMessage errorMessage = new ErrorMessage(message, ErrorLevel.Fatal);
                         ReportMessage(this, new MessageEventArgs(errorMessage));
-                        return results;
                     }
                 }
             }
@@ -138,40 +124,62 @@ namespace stageDamage
                 string message = "At this time, HEC-FDA does not allow a stage-damage compute without a frequency function. Stage-damage compute aborted" + Environment.NewLine;
                 ErrorMessage errorMessage = new ErrorMessage(message, ErrorLevel.Fatal);
                 ReportMessage(this, new MessageEventArgs(errorMessage));
-                return results;
             }
-
-            //TODO: Where do the results from the boundary profiles get computed?
-            //that is, which step?
-
-
-            //Hydraulic profiles will be sorted in descending exceedence probability (smallest profile is index 0) 
-            List < HydraulicProfile > profileList = _hydraulicDataset.HydraulicProfiles;
-
-
+        }
+        private PairedData CreateStageFrequency()
+        {
+            PairedData stageFrequency;
+            if (_AnalyticalFlowFrequency != null)
+            {
+                if (_DischargeStage != null)
+                {
+                    Tuple<double[], double[]> flowFreqAsTuple = _AnalyticalFlowFrequency.ToCoordinates();
+                    PairedData flowFrequencyPairedData = new PairedData(flowFreqAsTuple.Item1, flowFreqAsTuple.Item2);
+                    return stageFrequency = _DischargeStage.SamplePairedData(0.5).compose(flowFrequencyPairedData) as PairedData;
+                }
+            }
+            else if (_GraphicalFrequency != null)
+            {
+                if (_GraphicalFrequency.UsesStagesNotFlows)
+                {
+                    return stageFrequency = _GraphicalFrequency.SamplePairedData(0.5) as PairedData;
+                }
+                else
+                {
+                    if (_DischargeStage != null)
+                    {
+                        PairedData flowFrequencyPairedData = _GraphicalFrequency.SamplePairedData(0.5) as PairedData;
+                        return stageFrequency = _DischargeStage.SamplePairedData(0.5).compose(flowFrequencyPairedData) as PairedData;
+                    }
+                }
+            }
+            return null;
+        }
+        private void ComputeLowerStageDamage(PairedData stageFrequency, ref List<double> allStagesAtIndexLocation, ref List<ConsequenceDistributionResults> consequenceDistributionResults)
+        {
             //Part 1: Stages between min stage at index location and the stage at the index location for the lowest profile 
-            HydraulicProfile lowestProfile = profileList[0];
+            HydraulicProfile lowestProfile = _hydraulicDataset.HydraulicProfiles[0];
             float[] WSEAtLowest = lowestProfile.GetWSE(_inventory.GetPointMs());
             double stageAtProbabilityOfLowestProfile = stageFrequency.f(lowestProfile.Probability);
             //the delta is the difference between the min stage at the index location and the stage at the index location for the lowest profile 
-            float indexStationLowerStageDelta = (float)(stageAtProbabilityOfLowestProfile - minStage); 
-            int numIntermediateStagesToCompute = 15; //TODO: make this number meaningful.
+            float indexStationLowerStageDelta = (float)(stageAtProbabilityOfLowestProfile - _minStageForArea);
             //this interval defines the interval in stages by which we'll compute damage 
-            float interval = indexStationLowerStageDelta/ numIntermediateStagesToCompute;
+            float interval = indexStationLowerStageDelta / numIntermediateStagesToCompute;
             //Collect damage for first part of function 
-            for(int i = 0; i < numIntermediateStagesToCompute; i++)
+            for (int i = 0; i < numIntermediateStagesToCompute; i++)
             {
-                float[] WSEsParallelToIndexLocation = ExtrapolateFromBelowStagesAtIndexLocation(WSEAtLowest, interval, i, numIntermediateStagesToCompute);
+                float[] WSEsParallelToIndexLocation = ExtrapolateFromBelowStagesAtIndexLocation(WSEAtLowest, interval, i);
                 ConsequenceDistributionResults damageOrdinate = ComputeDamageOneCoordinate(seed, randomProvider, convergenceCriteria, _inventory, _occupancyTypes, _ImpactAreaID, WSEsParallelToIndexLocation);
                 consequenceDistributionResults.Add(damageOrdinate);
-                allStagesAtIndexLocation.Add(minStage + i * interval);
+                allStagesAtIndexLocation.Add(_minStageForArea + i * interval);
             }
-
+        }
+        private void ComputeMiddleStageDamage(PairedData stageFrequency, ref List<double> allStagesAtIndexLocation, ref List<ConsequenceDistributionResults> consequenceDistributionResults)
+        {
             //Part 2: Stages between the lowest profile and highest profile 
             //I think we do the 8 profiles for now 
             //then figure out how to interpolate later 
-
-            foreach (HydraulicProfile hydraulicProfile in profileList)
+            foreach (HydraulicProfile hydraulicProfile in _hydraulicDataset.HydraulicProfiles)
             {
                 double stageAtIndexLocation = stageFrequency.f(hydraulicProfile.Probability);
                 float[] stages = hydraulicProfile.GetWSE(_inventory.GetPointMs());
@@ -179,25 +187,42 @@ namespace stageDamage
                 consequenceDistributionResults.Add(damageOrdinate);
                 allStagesAtIndexLocation.Add(stageAtIndexLocation);
             }
-
+        }
+        private void ComputeUpperStageDamage(PairedData stageFrequency, ref List<double> allStagesAtIndexLocation, ref List<ConsequenceDistributionResults> consequenceDistributionResults)
+        {
             //Part 3: Stages between the highest profile 
-            float[] stagesAtHighestProfile = profileList[profileList.Count-1].GetWSE(_inventory.GetPointMs());
-            double stageAtProbabilityOfHighestProfile = stageFrequency.f(profileList[profileList.Count-1].Probability);
-            float indexStationUpperStageDelta = (float)(maxStage - stageAtProbabilityOfHighestProfile);
+            List<HydraulicProfile> profileList = _hydraulicDataset.HydraulicProfiles;
+
+            float[] stagesAtHighestProfile = profileList[profileList.Count - 1].GetWSE(_inventory.GetPointMs());
+            double stageAtProbabilityOfHighestProfile = stageFrequency.f(profileList[profileList.Count - 1].Probability);
+            float indexStationUpperStageDelta = (float)(_maxStageForArea - stageAtProbabilityOfHighestProfile);
             float upperInterval = indexStationUpperStageDelta / numIntermediateStagesToCompute;
 
             for (int i = 0; i < numIntermediateStagesToCompute; i++)
             {
-                float[] WSEsParallelToIndexLocation = ExtrapolateFromAboveAtIndexLocation(stagesAtHighestProfile, upperInterval, i, numIntermediateStagesToCompute);
+                float[] WSEsParallelToIndexLocation = ExtrapolateFromAboveAtIndexLocation(stagesAtHighestProfile, upperInterval, i);
                 ConsequenceDistributionResults damageOrdinate = ComputeDamageOneCoordinate(seed, randomProvider, convergenceCriteria, _inventory, _occupancyTypes, i, WSEsParallelToIndexLocation);
                 consequenceDistributionResults.Add(damageOrdinate);
-                allStagesAtIndexLocation.Add(maxStage - upperInterval * (numIntermediateStagesToCompute - i));
+                allStagesAtIndexLocation.Add(_maxStageForArea - upperInterval * (numIntermediateStagesToCompute - i));
             }
-            results = ConsequenceDistributionResults.ToUncertainPairedData(allStagesAtIndexLocation, consequenceDistributionResults);
-            return results;
+           
+            
         }
 
-        private float[] ExtrapolateFromAboveAtIndexLocation(float[] stagesAtHighestProfile, float upperInterval, int i, int numIntermediateStagesToCompute)
+        public List<UncertainPairedData> Compute()
+        {
+            List<double> allStagesAtIndexLocation = new List<double>();
+            List <ConsequenceDistributionResults> consequenceDistributionResults = new List<ConsequenceDistributionResults>();
+            PairedData stageFrequency = CreateStageFrequency();
+
+            ComputeLowerStageDamage(stageFrequency, ref allStagesAtIndexLocation, ref consequenceDistributionResults);
+            ComputeMiddleStageDamage(stageFrequency, ref allStagesAtIndexLocation, ref consequenceDistributionResults);
+            ComputeUpperStageDamage(stageFrequency, ref allStagesAtIndexLocation, ref consequenceDistributionResults);
+
+            List<UncertainPairedData> results = ConsequenceDistributionResults.ToUncertainPairedData(allStagesAtIndexLocation, consequenceDistributionResults);
+            return results;
+        }
+        private float[] ExtrapolateFromAboveAtIndexLocation(float[] stagesAtHighestProfile, float upperInterval, int i)
         {
             float[] extrapolatedStages = new float[stagesAtHighestProfile.Length];
             foreach (float stage in stagesAtHighestProfile)
@@ -206,8 +231,7 @@ namespace stageDamage
             }
             return extrapolatedStages;
         }
-
-        private float[] ExtrapolateFromBelowStagesAtIndexLocation(float[] WSEsAtLowest, float interval, int i, int numIntermediateStagesToCompute)
+        private float[] ExtrapolateFromBelowStagesAtIndexLocation(float[] WSEsAtLowest, float interval, int i)
         {
             float[] extrapolatedStages = new float[WSEsAtLowest.Length];
             foreach (float stage in WSEsAtLowest)
@@ -216,6 +240,8 @@ namespace stageDamage
             }
             return extrapolatedStages;
         }
+
+
 
         //public for testing
         //assume that the inventory has already been trimmed 
@@ -233,11 +259,6 @@ namespace stageDamage
                 iteration++;
             }
             return consequenceDistributionResults;
-        }
-
-        private void step1()
-        {
-
         }
         public void ReportMessage(object sender, MessageEventArgs e)
         {
