4c01740eca334f11fa8fa94651b4d6291a725308
Brennan Beam <BrennanBeam@gmail.com>
2022-10-03 16:02:51 -0700

    fix hydrualics test

--- fda-model-test/unittests/hydraulics/HydraulicProfileShould.cs
+++ fda-model-test/unittests/hydraulics/HydraulicProfileShould.cs
@@ -6,12 +6,13 @@ using HEC.FDA.Model.hydraulics;
 
 namespace fda_model_test.unittests
 {
+    [Trait("Category", "Unit")]
     public class HydraulicProfileShould
     {
-        private const string pathToNSIShapefile = @"..\..\..\Resources\MuncieNSI\MuncieNSI.shp";
-        private const string pathToIAShapefile = @"..\..\..\Resources\MuncieImpactAreas\ImpactAreas.shp";
-        private const string pathToResult = @"..\..\..\Resources\MuncieResult\Muncie.p04.hdf";
-        private const string pathToTerrain = @"..\..\..\Resources\MuncieTerrain\Terrain (1)_30ft_clip.hdf";
+        private const string pathToNSIShapefile = @"..\..\..\fda-model-test\Resources\MuncieNSI\MuncieNSI.shp";
+        private const string pathToIAShapefile = @"..\..\..\fda-model-test\Resources\MuncieImpactAreas\ImpactAreas.shp";
+        private const string pathToResult = @"..\..\..\fda-model-test\Resources\MuncieResult\Muncie.p04.hdf";
+        private const string pathToTerrain = @"..\..\..\fda-model-test\Resources\MuncieTerrain\Terrain (1)_30ft_clip.hdf";
 
         [Fact]
         public void GetWSEFromHDF()
@@ -24,7 +25,7 @@ namespace fda_model_test.unittests
             Inventory inventory = new Inventory(pathToNSIShapefile, pathToIAShapefile, map, occupancyTypes);
             float[] wses = profile.GetWSE(inventory.GetPointMs());
             Assert.Equal(696, wses.Length); // All structures have a value
-            Assert.Equal(947.244446, wses[0]); // first structure has correct WSE
+            Assert.Equal(947.244446, wses[0],1); // first structure has correct WSE
             Assert.Equal(-9999, wses[1]); // second structure is dry and reports -9999
         }
     }
