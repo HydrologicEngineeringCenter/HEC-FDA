22b92741640f9399b6c84040a4d60d092fb6c0a9
Brennan Beam <BrennanBeam@gmail.com>
2022-10-05 09:48:41 -0700

    adjusting tests to fit new pattern for hydraulic profiles

--- fda-model-test/unittests/hydraulics/HydraulicProfileShould.cs
+++ fda-model-test/unittests/hydraulics/HydraulicProfileShould.cs
@@ -12,13 +12,17 @@ namespace fda_model_test.unittests
         private const string pathToNSIShapefile = @"..\..\..\fda-model-test\Resources\MuncieNSI\MuncieNSI.shp";
         private const string pathToIAShapefile = @"..\..\..\fda-model-test\Resources\MuncieImpactAreas\ImpactAreas.shp";
         private const string pathToResult = @"..\..\..\fda-model-test\Resources\MuncieResult\Muncie.p04.hdf";
-        private const string pathToTerrain = @"..\..\..\fda-model-test\Resources\MuncieTerrain\Terrain (1)_30ft_clip.hdf";
+        private const string pathToGrid = @"..\..\..\fda-model-test\Resources\MuncieGrid\WSE (Max).Terrain.muncie_clip.tif";
 
-        [Fact]
-        public void GetWSEFromHDF()
+        [Theory]
+        [InlineData(pathToResult, HydraulicDataSource.UnsteadyHDF)]
+        [InlineData(pathToResult, HydraulicDataSource.SteadyHDF)]
+        //[InlineData(pathToGrid, HydraulicDataSource.WSEGrid)]
+
+        public void GetWSE(string path, HydraulicDataSource dataSource)
         {
             StructureInventoryColumnMap map = new StructureInventoryColumnMap();
-            HydraulicProfile profile = new HydraulicProfile(.01, pathToResult, HydraulicDataSource.UnsteadyHDF, "Max", pathToTerrain);
+            HydraulicProfile profile = new HydraulicProfile(.01, path, dataSource, "Max");
             //Empty (default) occupancy types
             OccupancyType occupancyType = new OccupancyType();
             List<OccupancyType> occupancyTypes = new List<OccupancyType>() { occupancyType };
@@ -26,7 +30,6 @@ namespace fda_model_test.unittests
             float[] wses = profile.GetWSE(inventory.GetPointMs());
             Assert.Equal(696, wses.Length); // All structures have a value
             Assert.Equal(947.244446, wses[0],1); // first structure has correct WSE
-            Assert.Equal(-9999, wses[1]); // second structure is dry and reports -9999
         }
     }
 }
