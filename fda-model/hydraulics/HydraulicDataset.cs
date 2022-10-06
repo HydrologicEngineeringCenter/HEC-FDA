1b9c8934e734f41a921b4671308feba94f341a1c
Brennan Beam <BrennanBeam@gmail.com>
2022-10-05 17:03:41 -0700

    add method to hydraulic dataset to correct stages ala Dean Method

--- fda-model/hydraulics/HydraulicDataset.cs
+++ fda-model/hydraulics/HydraulicDataset.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using HEC.FDA.Model.structures;
+using System.Collections.Generic;
 
 namespace HEC.FDA.Model.hydraulics
 {
@@ -10,6 +11,39 @@ namespace HEC.FDA.Model.hydraulics
             profiles.Sort();
             profiles.Reverse();
             HydraulicProfiles = profiles;
+        }
+        public static void CorrectDryStructureDepths(ref float[] wsesToCorrect, float[] firstFloorElevs, float[] nextProfileWses = null)
+        {
+            float offsetForDryStructures = 9;
+            float offsetForBarelyDryStructures = 2;
+            float dryCellValue = -9999;
+            if (nextProfileWses == null)
+            {
+                for (int i = 0; i < wsesToCorrect.Length; i++)
+                {
+                    //The case where the largest profile has dry structures
+                    if (wsesToCorrect[i] == dryCellValue)
+                    {
+                        wsesToCorrect[i] = (firstFloorElevs[i] - offsetForDryStructures);
+                    }
+                }
+            }
+            for (int i = 0; i < wsesToCorrect.Length; i++)
+            {
+                if (wsesToCorrect[i] == dryCellValue)
+                {
+                    //The case where the next largest profile is also dry
+                    if (nextProfileWses[i] == dryCellValue)
+                    {
+                        wsesToCorrect[i] = (firstFloorElevs[i] - offsetForDryStructures);
+                    }
+                    //The case where the next largest profile is not dry
+                    else
+                    {
+                        wsesToCorrect[i] = (firstFloorElevs[i] - offsetForBarelyDryStructures);
+                    }
+                }
+            }
         }
     }
 }
