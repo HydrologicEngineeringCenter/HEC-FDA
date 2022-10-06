11ed7a8733309b25562f2f82d4bc5fd37d2b0061
Brennan Beam <BrennanBeam@gmail.com>
2022-10-06 11:51:20 -0700

    add a simple histogram data provider

---
+++ ViewModel/TableWithPlot/Data/HistogramDataProvider.cs
@@ -0,0 +1,26 @@
+﻿using HEC.FDA.Model.paireddata;
+using HEC.FDA.ViewModel.TableWithPlot.Data.Abstract;
+using HEC.FDA.ViewModel.TableWithPlot.Data.ExtensionMethods;
+using HEC.FDA.ViewModel.TableWithPlot.Rows;
+using Statistics.Histograms;
+
+namespace HEC.FDA.ViewModel.TableWithPlot.Data
+{
+    internal class HistogramDataProvider : BaseDataProvider
+    {
+        public HistogramDataProvider(UncertainPairedData upd, bool isStrictMonotonic)
+        {
+            IsStrictMonotonic = isStrictMonotonic;
+            Name = "Empiracle";
+            for (int i = 0; i < upd.Xvals.Length; i++)
+            {
+                Data.Add(new HistogramRow(upd.Xvals[i], (Histogram)upd.Yvals[i], IsStrictMonotonic));
+            }
+            LinkList();
+        }
+        override public void AddUnlinkedRow(int i)
+        {
+           //do nothing. we wont add rows yet. 
+        }
+    }
+}
