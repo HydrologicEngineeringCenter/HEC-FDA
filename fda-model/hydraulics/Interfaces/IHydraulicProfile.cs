78bce6e4968aaee0603ad7ea6ff2f0285c581a44
Brennan Beam <BrennanBeam@gmail.com>
2022-11-14 16:21:58 -0800

    create interface for hydraulic profile

---
+++ fda-model/hydraulics/Interfaces/IHydraulicProfile.cs
@@ -0,0 +1,14 @@
+﻿using HEC.FDA.Model.hydraulics.enums;
+using RasMapperLib;
+using System.Xml.Linq;
+
+namespace HEC.FDA.Model.hydraulics.Interfaces
+{
+    public interface IHydraulicProfile
+    {
+        public double Probability { get; set; }
+        public float[] GetWSE(PointMs pts, HydraulicDataSource dataSource, string parentDirectory);
+        public XElement ToXML();
+
+    }
+}
