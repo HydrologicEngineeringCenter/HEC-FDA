abd522c2b7e811c5a3ac690d8af147b5a6c0bf68
Brennan Beam <BrennanBeam@gmail.com>
2022-11-14 16:22:57 -0800

    refactor tests to use Ihydraulicprofile

---
+++ fda-model-test/unittests/hydraulics/DummyHydraulicProfileShould.cs
@@ -0,0 +1,17 @@
+﻿using HEC.FDA.Model.hydraulics.Mock;
+using Xunit;
+
+namespace HEC.FDA.ModelTest.unittests.hydraulics
+{
+    public class DummyHydraulicProfileShould
+    {
+        [Fact]
+        void ReturnTheFlowsWeSet()
+        {
+            DummyHydraulicProfile prof = new DummyHydraulicProfile();
+            prof.Probability = .5f;
+            prof.DummyDepths = new float[] {0,1,2};
+            Assert.True(prof.DummyDepths == prof.GetWSE(null,Model.hydraulics.enums.HydraulicDataSource.UnsteadyHDF,null));
+        }
+    }
+}
