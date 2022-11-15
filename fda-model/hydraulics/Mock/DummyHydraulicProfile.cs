9ba2b065b8590d6dadad6e1b076a0a14a47197ae
Brennan Beam <BrennanBeam@gmail.com>
2022-11-15 11:18:33 -0800

    implement Ihydr. on dummy hydraulic profile.

--- fda-model/hydraulics/Mock/DummyHydraulicProfile.cs
+++ fda-model/hydraulics/Mock/DummyHydraulicProfile.cs
@@ -4,10 +4,30 @@ using System.Xml.Linq;
 
 namespace HEC.FDA.Model.hydraulics.Mock
 {
+    /// <summary>
+    /// This class exists explicitly for testing purposes. Should never go throught he UI. 
+    /// </summary>
     public class DummyHydraulicProfile : Interfaces.IHydraulicProfile
     {
         public float[] DummyDepths { get; set; }
         public double Probability { get; set; }
+        public string FileName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+        public string ProfileName { get => throw new System.NotImplementedException(); set => throw new System.NotImplementedException(); }
+
+        public int CompareTo(object obj)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public bool Equals(HydraulicProfile hydraulicProfileForComparison)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public string GetFilePath(string parentDirectory)
+        {
+            throw new System.NotImplementedException();
+        }
 
         public float[] GetWSE(PointMs pts, HydraulicDataSource dataSource, string parentDirectory)
         {
