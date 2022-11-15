796f83d7bd4015fdac9491465a23897b383b20d8
Brennan Beam <BrennanBeam@gmail.com>
2022-11-14 16:22:19 -0800

    create dummy hydraulic profile object

---
+++ fda-model/hydraulics/Mock/DummyHydraulicProfile.cs
@@ -0,0 +1,22 @@
+﻿using HEC.FDA.Model.hydraulics.enums;
+using RasMapperLib;
+using System.Xml.Linq;
+
+namespace HEC.FDA.Model.hydraulics.Mock
+{
+    public class DummyHydraulicProfile : Interfaces.IHydraulicProfile
+    {
+        public float[] DummyDepths { get; set; }
+        public double Probability { get; set; }
+
+        public float[] GetWSE(PointMs pts, HydraulicDataSource dataSource, string parentDirectory)
+        {
+            return DummyDepths;
+        }
+
+        public XElement ToXML()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
