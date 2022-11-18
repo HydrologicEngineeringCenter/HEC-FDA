ceb9de141142a26deccf861bf3bd36dd2c53a018
codymccoy <cody@rmanet.com>
2022-11-18 07:52:51 -0800

    Removed the print details menu item from the child node.

--- ViewModel/AggregatedStageDamage/AggregatedStageDamageElement.cs
+++ ViewModel/AggregatedStageDamage/AggregatedStageDamageElement.cs
@@ -1,13 +1,7 @@
 ﻿using HEC.FDA.ViewModel.Editors;
-using HEC.FDA.ViewModel.Hydraulics.GriddedData;
-using HEC.FDA.ViewModel.Inventory;
 using HEC.FDA.ViewModel.Utilities;
 using System;
 using System.Collections.Generic;
-using System.Data;
-using System.IO;
-using System.Text;
-using System.Windows;
 using System.Xml.Linq;
 
 namespace HEC.FDA.ViewModel.AggregatedStageDamage
@@ -43,7 +37,7 @@ namespace HEC.FDA.ViewModel.AggregatedStageDamage
             SelectedWSE = selectedWSE;
             SelectedStructures = selectedStructs;
 
-            AddActions();
+            AddDefaultActions(EditDamageCurve, StringConstants.EDIT_STAGE_DAMAGE_MENU);
         }
 
         public AggregatedStageDamageElement(XElement elementXML, int id):base(elementXML, id)
@@ -66,19 +60,10 @@ namespace HEC.FDA.ViewModel.AggregatedStageDamage
                 ImpactAreaFrequencyRows.Add(new ImpactAreaFrequencyFunctionRowItem(impAreaRow));
             }
 
-            AddActions();
+            AddDefaultActions(EditDamageCurve, StringConstants.EDIT_STAGE_DAMAGE_MENU);
         }
         #endregion
         #region Voids
-        private void AddActions()
-        {
-            AddDefaultActions(EditDamageCurve, StringConstants.EDIT_STAGE_DAMAGE_MENU);
-
-            NamedAction exportDetails = new NamedAction(this);
-            exportDetails.Header = StringConstants.EXPORT_STAGE_DAMAGE_MENU;
-            exportDetails.Action = ExportDetails;
-            Actions.Add(exportDetails);
-        }
 
         public void EditDamageCurve(object arg1, EventArgs arg2)
         {    
@@ -91,96 +76,6 @@ namespace HEC.FDA.ViewModel.AggregatedStageDamage
             string title = "Edit " + vm.Name;
             DynamicTabVM tab = new DynamicTabVM(title, vm, "EditStageDamageElement" + Name);
             Navigate(tab, false, true);
-        }
-
-        public void ExportDetails(object sender, EventArgs e)
-        {
-            if (IsManual)
-            {
-                MessageBox.Show("Details cannot be exported for manually entered stage damages.", "Unable to Export Details", MessageBoxButton.OK, MessageBoxImage.Information);
-            }
-            else
-            {
-                //List<string> structureDetails = scenarioStageDamage.ProduceStructureDetails();
-
-
-                //todo: Richard will write the logic that loads the table with the desired details.
-                string path = Storage.Connection.Instance.ProjectDirectory + "\\" + Name + "_Structure_Detail.csv";
-                using (var sw = new StreamWriter(path))
-                {
-                    WriteHeaderMetaData(sw);
-                    sw.WriteLine();
-                    WriteStructureTable(sw);
-                }// the streamwriter WILL be closed and flushed here, even if an exception is thrown.
-            }
-        }
-        private void WriteStructureTable(StreamWriter sw)
-        {
-            List<ChildElement> structureInventories = StudyCache.GetChildElementsOfType(typeof(InventoryElement));
-            List<ChildElement> wseElems = StudyCache.GetChildElementsOfType(typeof(HydraulicElement));
-
-            //I thought i would create a datatable object so that i could define columns and then create rows. I think this will make it 
-            //easier to update and maintain than just trying to write it out as a big string or something like that.
-            
-            //i thought we could grab the structure data from the inventory element form the study cache, but i don't see that option. We might
-            //have to use the persistence manager for structures and grab the table directly.
-
-            //all this code might get pretty long. We might want to move this to another class. ie: WriteDetailsFile(AggregatedStageDamageElement elem)
-
-            string structName = "struct name";
-            string structValue = "struct value";
-            DataTable dt = new DataTable();
-            dt.Columns.Add(structName);
-            dt.Columns.Add(structValue);
-
-            //InventoryElement inv = (InventoryElement)structureInventories[0];
-            //inv.StructureInventory.GetStructureData
-            for(int i = 0;i<20;i++)
-            {
-                DataRow dataRow = dt.NewRow();
-                dataRow[structName] = "testname " + i;
-                dataRow[structValue] = "testValue" + i * 100;
-                dt.Rows.Add(dataRow);
-            }
-
-            WriteDataTableToCSV(dt, sw);
-        }
-
-        private void WriteDataTableToCSV(DataTable dt, StreamWriter sw)
-        {
-            StringBuilder sb = new StringBuilder();
-
-            foreach (DataColumn col in dt.Columns)
-            {
-                sb.Append(col.ColumnName + ',');
-            }
-
-            sb.Remove(sb.Length - 1, 1);
-            sb.Append(Environment.NewLine);
-
-            foreach (DataRow row in dt.Rows)
-            {
-                for (int i = 0; i < dt.Columns.Count; i++)
-                {
-                    sb.Append(row[i].ToString() + ",");
-                }
-
-                sb.Append(Environment.NewLine);
-            }
-            sw.WriteLine(sb.ToString());
-        }
-
-        private void WriteHeaderMetaData(StreamWriter sw)
-        {
-            Study.StudyPropertiesElement propElem = StudyCache.GetStudyPropertiesElement();
-            sw.Write("Study: ");
-            sw.WriteLine(Path.GetFileName(propElem.StudyPath));
-            
-            sw.Write("Description: ");
-            sw.WriteLine(propElem.Description);
-
-            sw.Write("Created: ");
-            sw.WriteLine(propElem.CreatedDate);
         }
 
         public override XElement ToXML()
