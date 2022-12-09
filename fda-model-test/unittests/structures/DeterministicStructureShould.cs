5882b1eea53c647ec0d1bcd66b5f6d839924d832
rnugent3 <richard.nugent.3@gmail.com>
2022-12-09 09:51:01 -0800

    added tests on number of structures price index and year in service

--- fda-model-test/unittests/structures/DeterministicStructureShould.cs
+++ fda-model-test/unittests/structures/DeterministicStructureShould.cs
@@ -22,48 +22,27 @@ namespace HEC.FDA.ModelTest.unittests.structures
         private static SampledStructureParameters sampledStructureParameters = new SampledStructureParameters(occupancyTypeName, occupancyTypeDamageCategory, structureDepthPercentDamage, sampledFirstFloorElevation, sampledStructureValue, computeContentDamage, computeVehicleDamage, computeOtherDamage, contentDepthPercentDamage, sampledContentValue);
         private static int structureID = 44;
         private static int impactAreaID = 55;
-
-
-        [Theory]
-        [InlineData(100, 0, 0, -999)]
-        [InlineData(104, 400, 200, 4)]//begDamDept = depthabovefoundationHeight -- positive damages
-        [InlineData(108, 800, 400, 8)]//begDamDept = depthabovefoundationHeight -- positive damages
-        public void DeterministicStructureShouldComputeDamageCorrectly(float waterSurfaceElevation, double expectedStructureDamage, double expectedContentDamage, double beginningDamageDepth)
-        {
-            DeterministicStructure deterministicStructure = new DeterministicStructure(structureID, impactAreaID, sampledStructureParameters, beginningDamageDepth);
-
-            ConsequenceResult consequenceResult = deterministicStructure.ComputeDamage(waterSurfaceElevation);
-            Assert.Equal(expectedStructureDamage, consequenceResult.StructureDamage, 0);
-            Assert.Equal(expectedContentDamage, consequenceResult.ContentDamage, 0);
-        }
+        private static int yearInService = 1820;
 
         [Theory]
-        [InlineData(100, 0, 0, -999)]
-        [InlineData(104, 0, 0, 4.1)]//begDamDept > depthabovefoundationHeight -- zero damages
-        [InlineData(108, 0, 0, 8.1)]//begDamDept > depthabovefoundationHeight -- zero damages
-        public void DeterministicStructureShouldComputeDamageCorrectly2(float waterSurfaceElevation, double expectedStructureDamage, double expectedContentDamage, double beginningDamageDepth)
+        [InlineData(100, 0, 0, -999, 1, 1900, 1)]//begDamDept is not in the inventory
+        [InlineData(104, 400, 200, 4, 1, 1900, 1)]//begDamDept = depthabovefoundationHeight -- positive damages
+        [InlineData(108, 0, 0, 8, 1, 1800, 1)]//analysis year is before year in service, damage should be zero 
+        [InlineData(104, 400, 200, 4, 2, 1900, 1)]//begDamDept = depthabovefoundationHeight -- positive damages
+        [InlineData(108, 800, 400, 8, 2, 1900, 1)]//begDamDept = depthabovefoundationHeight -- positive damages
+        [InlineData(104, 400, 200, 4, 1, 1900, 2)]//begDamDept = depthabovefoundationHeight -- positive damages
+        [InlineData(108, 800, 400, 8, 1, 1900, 3)]//begDamDept = depthabovefoundationHeight -- positive damages
+        [InlineData(104, 0, 0, 4.1, 1, 1900, 1)]//begDamDept > depthabovefoundationHeight -- zero damages
+        [InlineData(108, 0, 0, 8.1, 1, 1900, 2)]//begDamDept > depthabovefoundationHeight -- zero damages
+        [InlineData(104, 400, 200, 3.9, 2, 1900, 1)]//begDamDept < depthabovefoundationHeight -- positive damages
+        [InlineData(108, 800, 400, 7.9, 1, 1900, 3)]//begDamDept < depthabovefoundationHeight -- positive damages
+        public void ComputeDamageCorrectlyBegDamDepthEqualToDepthAboveFFE(float waterSurfaceElevation, double expectedStructureDamage, double expectedContentDamage, double beginningDamageDepth, double priceIndex, int analysisYear, int numberOfStructures)
         {
-            DeterministicStructure deterministicStructure = new DeterministicStructure(structureID, impactAreaID, sampledStructureParameters, beginningDamageDepth);
+            DeterministicStructure deterministicStructure = new DeterministicStructure(structureID, impactAreaID, sampledStructureParameters, beginningDamageDepth, numberOfStructures, yearInService);
 
-            ConsequenceResult consequenceResult = deterministicStructure.ComputeDamage(waterSurfaceElevation);
-            Assert.Equal(expectedStructureDamage, consequenceResult.StructureDamage, 0);
-            Assert.Equal(expectedContentDamage, consequenceResult.ContentDamage, 0);
+            ConsequenceResult consequenceResult = deterministicStructure.ComputeDamage(waterSurfaceElevation, priceIndex, analysisYear);
+            Assert.Equal(expectedStructureDamage * numberOfStructures * priceIndex, consequenceResult.StructureDamage, 0);
+            Assert.Equal(expectedContentDamage * numberOfStructures * priceIndex, consequenceResult.ContentDamage, 0);
         }
-
-        [Theory]
-        [InlineData(100, 0, 0, -999)]
-        [InlineData(104, 400, 200, 3.9)]//begDamDept < depthabovefoundationHeight -- positive damages
-        [InlineData(108, 800, 400, 7.9)]//begDamDept < depthabovefoundationHeight -- positive damages
-        public void DeterministicStructureShouldComputeDamageCorrectly3(float waterSurfaceElevation, double expectedStructureDamage, double expectedContentDamage, double beginningDamageDepth)
-        {
-            DeterministicStructure deterministicStructure = new DeterministicStructure(structureID, impactAreaID, sampledStructureParameters, beginningDamageDepth);
-
-            ConsequenceResult consequenceResult = deterministicStructure.ComputeDamage(waterSurfaceElevation);
-            Assert.Equal(expectedStructureDamage, consequenceResult.StructureDamage, 0);
-            Assert.Equal(expectedContentDamage, consequenceResult.ContentDamage, 0);
-        }
-
-
-
     }
 }
