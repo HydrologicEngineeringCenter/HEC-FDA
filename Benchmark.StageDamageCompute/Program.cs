5abbf75ec9b0a9d99115feb6f0ad5b8603d41c53
Brennan Beam <BrennanBeam@gmail.com>
2022-12-21 14:25:54 -0800

    Change debug settings for profiling

---
+++ Benchmark.StageDamageCompute/Program.cs
@@ -0,0 +1,6 @@
+﻿using HEC.FDA.Model.compute;
+using HEC.FDA.ModelTest.integrationtests;
+var stageDamageTestClass = new TractableStageDamageTests();
+stageDamageTestClass.impactAreaStageDamage.Compute(new MedianRandomProvider());
+Console.WriteLine("compute complete");
+
