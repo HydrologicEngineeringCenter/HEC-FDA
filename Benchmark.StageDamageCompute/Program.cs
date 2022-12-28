db63522c696f18d2bf5bc42e3587926a43a75116
Brennan Beam <BrennanBeam@gmail.com>
2022-12-28 12:47:23 -0800

    setup new test

--- Benchmark.StageDamageCompute/Program.cs
+++ Benchmark.StageDamageCompute/Program.cs
@@ -1,6 +1,15 @@
 ﻿using HEC.FDA.Model.compute;
 using HEC.FDA.ModelTest.integrationtests;
-var stageDamageTestClass = new TractableStageDamageTests();
-stageDamageTestClass.impactAreaStageDamage.Compute(new MedianRandomProvider());
-Console.WriteLine("compute complete");
+using HEC.FDA.ModelTest.unittests;
+using Statistics;
 
+//Testing TractableStageDamage
+//var stageDamageTestClass = new TractableStageDamageTests();
+//stageDamageTestClass.impactAreaStageDamage.Compute(new MedianRandomProvider());
+//Console.WriteLine("compute complete");
+
+var testClass = new StageDamageShould();
+for( int i = 0; i < 5; i++)
+{
+    testClass.ComputeDamageOneCoordinateShouldComputeCorrectly(560, 504, 690, 621);
+}
