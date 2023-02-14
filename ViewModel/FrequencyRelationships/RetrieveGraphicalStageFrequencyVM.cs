73dfe8b03e6119f2606783a1b1996ac25180e98a
Brennan Beam <BrennanBeam@gmail.com>
2023-02-13 16:53:20 -0800

    create control and vm

---
+++ ViewModel/FrequencyRelationships/RetrieveGraphicalStageFrequencyVM.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEC.FDA.ViewModel.FrequencyRelationships
+{
+    internal class RetrieveGraphicalStageFrequencyVM: MVVMFramework.ViewModel.Implementations.ValidatingBaseViewModel
+    {
+
+    }
+}
