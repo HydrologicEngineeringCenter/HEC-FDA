c3bfd736b414f9b3d32912417b655cfef68641f9
Brennan Beam <BrennanBeam@gmail.com>
2023-02-28 13:25:30 -0800

    updated with the latest changes from framework.

--- HEC.MVVMFramework.Model/Messaging/ValidationGroup.cs
+++ HEC.MVVMFramework.Model/Messaging/ValidationGroup.cs
@@ -11,17 +11,18 @@ namespace HEC.MVVMFramework.Model.Messaging
     public class ValidationGroup : Validation, IReportMessage
     {
         private const string GROUP_INDENTATION = "    ";
-        private List<ValidationErrorLogger> _ValidationObjects;
-        private List<string> _IntroMessages;
+        private List<ValidationErrorLogger> _ValidationObjects = new List<ValidationErrorLogger>();
+        private List<string> _IntroMessages = new List<string>();
         private string _IntroMessage;
 
         public event MessageReportedEventHandler MessageReport;
 
         public List<ValidationGroup> ChildGroups { get; set; } = new List<ValidationGroup>();
-   
-        public ValidationGroup(List<IValidate> validationObjects, string intoMessage)
+
+        public ValidationGroup(string intoMessage)
         {
             MessageHub.Register(this);
+            _IntroMessage = intoMessage;
         }
 
         public ValidationGroup(List<ValidationErrorLogger> validationObjects, List<string> introMessages, string introMessage)
@@ -30,30 +31,16 @@ namespace HEC.MVVMFramework.Model.Messaging
             _ValidationObjects = validationObjects;
             _IntroMessages = introMessages;
             _IntroMessage = introMessage;
-            
         }
-
-        public ValidationGroup(List<Tuple<IValidate, string>> validationObjects, string introMessage = null)
-        {
-            MessageHub.Register(this);
-
-
-        }
-
-        ValidationGroup(List<ValidationGroup> validationGroups, string introMessage = null)
-        {
-            MessageHub.Register(this);
-        }
-
 
         public string GetErrorMessages()
         {
             string errorMsg = null;
             StringBuilder sb = new StringBuilder();
-            for(int i = 0; i < _ValidationObjects.Count; i++)
+            for (int i = 0; i < _ValidationObjects.Count; i++)
             {
                 string msg = _ValidationObjects[i].GetErrorMessages(_IntroMessages[i]);
-                if(msg != null)
+                if (msg != null)
                 {
                     //add indent to each line
                     msg = GROUP_INDENTATION + msg.Replace(Environment.NewLine, Environment.NewLine + GROUP_INDENTATION);
@@ -62,7 +49,7 @@ namespace HEC.MVVMFramework.Model.Messaging
             }
 
             //now handle sub groups:
-            foreach(ValidationGroup vg in ChildGroups)
+            foreach (ValidationGroup vg in ChildGroups)
             {
                 string msg = vg.GetErrorMessages();
                 if (msg != null)
@@ -73,32 +60,12 @@ namespace HEC.MVVMFramework.Model.Messaging
                 }
             }
 
-            if(sb.Length != 0)
+            if (sb.Length != 0)
             {
                 errorMsg = _IntroMessage + Environment.NewLine + Environment.NewLine + sb.ToString();
             }
             return errorMsg;
 
-            //ErrorLevel errorLevel = GetHighestErrorLevel(_ValidationObjects);
-            //if (errorLevel > ErrorLevel.Unassigned)
-            //{
-            //    StringBuilder sb = new StringBuilder();
-            //    //ReportMessage(this, new MessageEventArgs(new ErrorMessage(_IntroMessage, errorLevel)));
-            //    sb.AppendLine(_IntroMessage);
-            //    for (int i = 0; i < _ValidationObjects.Count; i++)
-            //    {
-            //        ValidationErrorLogger validationObj = _ValidationObjects[i];
-            //        string valObjIntroMessage = _IntroMessages[i];
-            //        if (valObjIntroMessage != null)
-            //        {
-            //            validationObj.LogErrors(GROUP_INDENTATION + valObjIntroMessage);
-            //        }
-            //        else
-            //        {
-            //            validationObj.LogErrors(GROUP_INDENTATION);
-            //        }
-            //    }
-            //}
         }
 
         //can i override the has errors and get errors and error level etc?
