9eefa0df336aafa207f867dab4b9c847b4fdff12
Brennan Beam <BrennanBeam@gmail.com>
2023-03-30 15:24:31 -0700

    convert to oxyplot. fix #831

--- HEC.FDA.ViewModel/ImpactAreaScenario/Results/DamageWithUncertaintyVM.cs
+++ HEC.FDA.ViewModel/ImpactAreaScenario/Results/DamageWithUncertaintyVM.cs
@@ -1,21 +1,17 @@
 ﻿using HEC.Plotting.SciChart2D.DataModel;
-using HEC.Plotting.SciChart2D.ViewModel;
 using System.Collections.Generic;
 using HEC.FDA.ViewModel.ImpactAreaScenario.Results.RowItems;
-using System.Linq;
-using Statistics.Histograms;
-using HEC.FDA.ViewModel.Utilities;
 using HEC.FDA.Model.metrics;
 using Statistics.Distributions;
+using OxyPlot;
+using OxyPlot.Series;
+using OxyPlot.Axes;
 
 namespace HEC.FDA.ViewModel.ImpactAreaScenario.Results
 {
     public class DamageWithUncertaintyVM : BaseViewModel
     {
-        private HistogramData2D _data;
-        public SciChart2DChartViewModel ChartViewModel { get; set; } = new SciChart2DChartViewModel("Damage Uncertainty");
-        public bool HistogramVisible { get; set; } = true;
-
+        public PlotModel MyPlot { get; set; } = new PlotModel();
         public List<EadRowItem> Rows { get; } = new List<EadRowItem>();
         public double Mean { get; set; }
         public DamageWithUncertaintyVM(ScenarioResults scenarioResults, int impactAreaID)
@@ -23,7 +19,7 @@ namespace HEC.FDA.ViewModel.ImpactAreaScenario.Results
             ImpactAreaScenarioResults iasResult = scenarioResults.GetResults(impactAreaID);
             Mean = iasResult.MeanExpectedAnnualConsequences(impactAreaID: impactAreaID);
 
-            LoadHistogram(iasResult);
+            DefineHistogramSettings(iasResult);
 
             List<double> qValues = new List<double>();
             qValues.Add(scenarioResults.ConsequencesExceededWithProbabilityQ(.75, impactAreaID));
@@ -33,20 +29,33 @@ namespace HEC.FDA.ViewModel.ImpactAreaScenario.Results
             loadTableValues(qValues);
         }
 
-        private void LoadHistogram(ImpactAreaScenarioResults iasResult)
+        private void DefineHistogramSettings(ImpactAreaScenarioResults iasResult)
         {
             Empirical empirical = iasResult.ConsequenceResults.GetAggregateEmpiricalDistribution(impactAreaID: iasResult.ImpactAreaID);
-            if (empirical.CumulativeProbabilities.Length <= 1)
+            (double min, double valueStep, double[] cumulativeRelativeFrequencies) = empirical.ComputeCumulativeFrequenciesForPlotting();
+            double[] damageValues = new double[cumulativeRelativeFrequencies.Length];
+
+            double damageValue = min;
+            for (int i =0; i< cumulativeRelativeFrequencies.Length;i++)
             {
-                HistogramVisible = false;
+                damageValues[i] = (damageValue);
+                damageValue += valueStep;
             }
-            else
+
+            PlotModel model = new PlotModel();
+            model.Title = "EAD Distribution";
+            double MajorAxisTickInterval = (empirical.Max- empirical.Min)/5;
+            model.Axes.Add(new LinearAxis() { Title = "Expected Annual Damage ($)", Position = AxisPosition.Bottom, MajorStep = MajorAxisTickInterval });
+            model.Axes.Add(new LinearAxis() { Title = "Frequency", Position = AxisPosition.Left});
+            var lineSeries = new LineSeries();
+            for(int i =0; i < cumulativeRelativeFrequencies.Length; i++)
             {
-                (double min, double valueStep, double[] cumulativeRelativeFrequencies) = empirical.ComputeCumulativeFrequenciesForPlotting();
-                _data = new HistogramData2D(valueStep, min, cumulativeRelativeFrequencies, "Chart", "Cumulative Relative Frequency", StringConstants.HISTOGRAM_VALUE, StringConstants.HISTOGRAM_FREQUENCY);
-                HistogramColor.SetHistogramColor(_data);
-                ChartViewModel.LineData.Set(new List<SciLineData>() { _data });
+                lineSeries.Points.Add(new DataPoint(damageValues[i], cumulativeRelativeFrequencies[i]));
             }
+            lineSeries.Title = "EAD Distribution";
+            model.Series.Add(lineSeries);
+            MyPlot = model;
+            MyPlot.InvalidatePlot(true);
         }
 
         private void loadTableValues(List<double> qValues)
@@ -62,7 +71,7 @@ namespace HEC.FDA.ViewModel.ImpactAreaScenario.Results
                     rows.Add(new EadRowItem(xValNames[i], qValues[i]));
                 }
             }
-            Rows.AddRange( rows);
+            Rows.AddRange(rows);
         }
 
     }
