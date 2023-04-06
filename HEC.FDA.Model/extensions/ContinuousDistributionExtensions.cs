0784f6dce14dd46070bc5e3eebd74dde5b18d881
Brennan Beam <BrennanBeam@gmail.com>
2023-04-06 14:35:43 -0700

    bug fixes in continuous distribution

--- HEC.FDA.Model/extensions/ContinuousDistributionExtensions.cs
+++ HEC.FDA.Model/extensions/ContinuousDistributionExtensions.cs
@@ -2,34 +2,29 @@
 using HEC.FDA.Model.paireddata;
 using Statistics;
 using Statistics.Histograms;
+using System;
 
 namespace HEC.FDA.Model.extensions
 {
     public static class ContinuousDistributionExtensions
     {
-        public static PairedData BootstrapToPairedData(this ContinuousDistribution continuousDistribution, IProvideRandomNumbers randomProvider, double[] ExceedanceProbabilities, int ordinatesToSample = 0)
+        public static PairedData BootstrapToPairedData(this ContinuousDistribution continuousDistribution, IProvideRandomNumbers randomProvider, double[] ExceedanceProbabilities)
         {
+            Array.Sort(ExceedanceProbabilities);
             double[] samples;
-            if (ordinatesToSample <= 0)
-            {
-                ordinatesToSample = (int)continuousDistribution.SampleSize;
-            }
-            samples = randomProvider.NextRandomSequence(ordinatesToSample);
+            samples = randomProvider.NextRandomSequence(continuousDistribution.SampleSize);
             IDistribution bootstrap = continuousDistribution.Sample(samples);
-            double[] x = new double[ExceedanceProbabilities.Length];
             double[] y = new double[ExceedanceProbabilities.Length];
             for (int i = 0; i < ExceedanceProbabilities.Length; i++)
             {
-                //same exceedance probs as graphical and as 1.4.3
-                double prob = 1 - ExceedanceProbabilities[i];
-                x[i] = prob;
-                //y values in increasing order 
-                y[i] = bootstrap.InverseCDF(prob);
+                //y values in decreasing order 
+                // "1-x" because LP3 is done in Non-Exceedence Probabilities, but we want to speak in exceedence. The 500YR flood should have a tiny probability associated with it. Not a large one 
+                y[i] = bootstrap.InverseCDF(1-ExceedanceProbabilities[i]);
             }
-            return new PairedData(x, y);
+            return new PairedData(ExceedanceProbabilities, y);
         }
 
-        public static UncertainPairedData BootstrapToUncertainPairedData(this ContinuousDistribution continuousDistribution, IProvideRandomNumbers randomProvider, double[] ExceedanceProbabilities,int realizations , double histogramBinWidth, int ordinatesToSample = 0)
+        public static UncertainPairedData BootstrapToUncertainPairedData(this ContinuousDistribution continuousDistribution, IProvideRandomNumbers randomProvider, double[] ExceedanceProbabilities,int realizations = 10000 , double histogramBinWidth = 0.5 )
         {
             Histogram[] ys = new Histogram[ExceedanceProbabilities.Length];
             for (int iterator = 0; iterator < ys.Length; iterator++)
@@ -39,10 +34,12 @@ namespace HEC.FDA.Model.extensions
 
             for(int i=0; i<realizations; i++)
             {
-                PairedData pd = continuousDistribution.BootstrapToPairedData( randomProvider, ExceedanceProbabilities, ordinatesToSample);
-                for(int j=0;j<ExceedanceProbabilities.Length-1;j++)
+                PairedData pd = continuousDistribution.BootstrapToPairedData( randomProvider, ExceedanceProbabilities);
+                for(int j=0;j<ExceedanceProbabilities.Length;j++)
                 {
-                    ys[j].AddObservationToHistogram(pd.f(ExceedanceProbabilities[j]));
+                    double prob = ExceedanceProbabilities[j];
+                    double flow = pd.f(prob); 
+                    ys[j].AddObservationToHistogram(flow) ;
                 }
             }
             return new UncertainPairedData(ExceedanceProbabilities, ys, new CurveMetaData("Exceedance Probs", "Flow Histograms"));
