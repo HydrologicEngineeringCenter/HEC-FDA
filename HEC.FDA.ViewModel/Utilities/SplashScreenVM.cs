f111e9b93217b5c1e452af6dc6669cea8e3e6c4d
codymccoy <cody@rmanet.com>
2023-05-22 08:52:20 -0700

    Added splash screen

---
+++ HEC.FDA.ViewModel/Utilities/SplashScreenVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEC.FDA.ViewModel.Utilities
+{
+    public class SplashScreenVM : BaseViewModel
+    {
+
+
+    }
+}
