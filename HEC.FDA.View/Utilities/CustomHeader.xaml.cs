fbd2db03be991c4e4670af02083d286d3d4f127e
Brennan Beam <BrennanBeam@gmail.com>
2023-08-15 16:18:12 -0700

    get rid of binding errors for null and empty image sources.

--- HEC.FDA.View/Utilities/CustomHeader.xaml.cs
+++ HEC.FDA.View/Utilities/CustomHeader.xaml.cs
@@ -2,23 +2,11 @@
 
 namespace HEC.FDA.View.Utilities
 {
-    /// <summary>
-    /// Interaction logic for CustomHeader.xaml
-    /// </summary>
     public partial class CustomHeader : UserControl
     {
         public CustomHeader()
         {
             InitializeComponent();
         }
-        //public CustomHeader(string name, ImageSource image, string decoration = "")
-        //{
-        //    InitializeComponent();
-
-        //    txt_Name.Text = name;
-        //    txt_Decoration.Text = decoration;
-        //    image_Image.Source = image;
-
-        //}
     }
 }
