693f384fc91ddf1c8e42eb2bb0869ded0bcd12c3
Brennan Beam <BrennanBeam@gmail.com>
2023-09-27 10:09:34 -0700

    add missing cast in hydraulic profile

--- HEC.FDA.Model/hydraulics/HydraulicProfile.cs
+++ HEC.FDA.Model/hydraulics/HydraulicProfile.cs
@@ -62,7 +62,7 @@ namespace HEC.FDA.Model.hydraulics
             {
                 return new float[pts.Count];
             }
-            RasterPyramid<float> baseRaster = baseDs.AsRasterizer();
+            RasterPyramid<float> baseRaster = (RasterPyramid<float>)baseDs.AsRasterizer();
 
             List<Geospatial.Vectors.Point> geospatialpts = RasMapperLib.Utilities.Converter.Convert(pts);
             Memory<Geospatial.Vectors.Point> points = new Memory<Geospatial.Vectors.Point>(geospatialpts.ToArray());
