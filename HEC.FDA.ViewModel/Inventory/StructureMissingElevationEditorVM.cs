289ba11236811d728882d0a1c7aba15888f25e6a
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2023-11-17 15:33:49 -0800

    Remove unused properties.

--- HEC.FDA.ViewModel/Inventory/StructureMissingElevationEditorVM.cs
+++ HEC.FDA.ViewModel/Inventory/StructureMissingElevationEditorVM.cs
@@ -4,25 +4,8 @@ using System.Data;
 namespace HEC.FDA.ViewModel.Inventory
 {
     public class StructureMissingElevationEditorVM : BaseViewModel
-    {
-        private bool _UsingFirstFloorElevation;
-        private bool _ElevsFromTerrainFile;
-
+    { 
         public DataTable MissingDataTable { get; }
-
-        public List<StructureMissingDataRowItem> Rows { get; } = new List<StructureMissingDataRowItem>();
-        public bool UsingFirstFloorElevation
-        {
-            get { return _UsingFirstFloorElevation; }
-            set { _UsingFirstFloorElevation = value; NotifyPropertyChanged(); }
-        }
-        public bool ElevsFromTerrainFile
-        {
-            get { return _ElevsFromTerrainFile; }
-            set { _ElevsFromTerrainFile = value; NotifyPropertyChanged(); }
-        }
-
-        public bool FromStructureFile { get; set; }
 
         public StructureMissingElevationEditorVM(List<StructureMissingDataRowItem> rows,  InventoryColumnSelectionsVM inventoryColumnSelectionsVM)
         {
