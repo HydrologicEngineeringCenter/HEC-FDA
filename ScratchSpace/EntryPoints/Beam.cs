510b1fc7ac0bafc5309f8096579df6000f280de8
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-07-24 16:34:52 -0700

    establishes scratch space.

---
+++ ScratchSpace/EntryPoints/Beam.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ScratchSpace.EntryPoints;
+public static class Beam
+{
+    public static void EntryPoint()
+    {
+
+    }
+}
---
+++ ScratchSpace/Program.cs
@@ -0,0 +1,12 @@
+﻿using ScratchSpace.EntryPoints;
+
+const string BrennanWork = "EIWRW4EFAANB144";
+const string BrennanVM = "DESKTOP-U5B7M40";
+
+string pcName = GetPCName();
+
+if (pcName.Equals(BrennanWork) || pcName.Equals(BrennanVM))
+{
+    Beam.EntryPoint();
+}
+static string GetPCName() => Environment.GetEnvironmentVariable("COMPUTERNAME");
