99c38a7f6c83c24ae09d576a9c795a5ad23a730e
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-08-21 13:56:54 -0700

    fix bug in checking projection is set.

--- HEC.FDA.ViewModel/Inventory/ImportStructuresFromShapefileVM.cs
+++ HEC.FDA.ViewModel/Inventory/ImportStructuresFromShapefileVM.cs
@@ -174,7 +174,7 @@ namespace HEC.FDA.ViewModel.Inventory
 
         public bool NextButtonClicked()
         {
-            if(Storage.Connection.Instance.ProjectionFile == null && ((TerrainElement)StudyCache.GetParentElementOfType<TerrainOwnerElement>().Elements[0] == null))
+            if(string.IsNullOrWhiteSpace(Storage.Connection.Instance.ProjectionFile) && ((TerrainElement)StudyCache.GetParentElementOfType<TerrainOwnerElement>().Elements[0] == null))
             {
                 System.Windows.MessageBox.Show("Please set your project projection in the study properties.", "Missing Projection", MessageBoxButton.OK, MessageBoxImage.Error);
                 return false;
