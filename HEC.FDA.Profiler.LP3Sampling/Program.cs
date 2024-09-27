205826ffa1b27c4744e1b9cb5f6ac8548ced1458
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-09-27 11:35:04 -0700

    removes profiler project. Scratch space should serve this role from now on

--- HEC.FDA.Profiler.LP3Sampling/Program.cs
+++
@@ -1,6 +0,0 @@
-﻿using HEC.FDA.Model.compute;
-using HEC.FDA.Model.extensions;
-using Statistics.Distributions;
-
-LogPearson3 lp3 = new LogPearson3(3.5, 0.22, 0.1, 60);
-lp3.BootstrapToUncertainPairedData(new RandomProvider(1234), LogPearson3._RequiredExceedanceProbabilitiesForBootstrapping, 1000000, 0.5); 
