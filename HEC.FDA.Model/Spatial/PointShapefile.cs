3f99abc365e9899fccddd3e15072147e464275b6
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-10-08 13:35:46 -0700

    group and create spatial classes

---
+++ HEC.FDA.Model/Spatial/PointShapefile.cs
@@ -0,0 +1,45 @@
+﻿using Geospatial.Features;
+using Geospatial.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Extensions;
+using Utility.Logging;
+using Utility.Memory;
+
+namespace HEC.FDA.Model.Spatial;
+public class PointShapefile
+{
+    private const string SHAPEFILE_LOAD_FAILED = "The point shapefile has not been loaded. Call LoadStructureShapefile() first";
+    private const string SHAPEFILE_CORRUPT = "The point shapefile failed to load correctly.";
+    private PointFeatureCollection _pointCollection;
+
+    public IReadOnlyList<TableRow> Rows { get => _pointCollection.AttributeTable.Rows; }
+    public string[] ColumnNames { get => _pointCollection.AttributeTable.Columns.Select((c) => c.Name).ToArray(); }
+
+    public PointShapefile(string pointShapefilePath)
+    {
+        OperationResult res = ShapefileWriter.TryReadShapefile(pointShapefilePath, out _pointCollection);
+        if (!res) 
+        {
+            throw new Exception(SHAPEFILE_CORRUPT);
+        }
+    }
+
+    /// <summary>
+    /// Gets the row values as objects for the row of the specified id.
+    /// </summary>
+    public object[] GetRowAsObjects(int id)
+    {
+        string[] columns = _pointCollection.AttributeTable.Columns.Select((c) => c.Name).ToArray();
+        var row = Rows[id];
+        object[] rowValues = new object[columns.Length];
+        for (int i = 0; i < columns.Length; i++)
+        {
+            rowValues[i] = row.Value(columns[i]);
+        }
+        return rowValues;
+    }
+}
