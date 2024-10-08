bfabfc196544d0ce932ff23a571e2c530877d190
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-10-08 16:12:05 -0700

    tests and bug fixes for pointshapefiles and sdv

---
+++ HEC.FDA.ModelTest/unittests/StructureDataValidatorShould.cs
@@ -0,0 +1,72 @@
+﻿using HEC.FDA.Model.Spatial;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Utility.Memory;
+using Xunit;
+
+namespace HEC.FDA.ModelTest.unittests;
+public class StructureDataValidatorShould
+{
+    static PointShapefile pointShapefile = new PointShapefile(Resources.StringResourcePaths.pathToNSIShapefile);
+    [Fact]
+    public void RowHasValuesForColumns_AllFieldsPresent_ReturnsTrue()
+    {
+        //Act
+        bool valid = StructureDataValidator.RowHasValuesForColumns(pointShapefile, 0, new List<string> { "fd_id", "occtype", "yrbuilt" }, out List<string> missingValues);
+
+        //Assert
+        Assert.True(valid);
+        Assert.True(missingValues.Count == 0);
+    }
+
+    [Fact]
+    public void RowHasValuesForColumns_MissingFields_ReturnsFalse()
+    {
+        bool valid = StructureDataValidator.RowHasValuesForColumns(pointShapefile, 0, ["teachers"], out List<string> columnsWithMissingData);
+        Assert.False(valid);
+        Assert.True(columnsWithMissingData.Count == 1);
+    }
+
+    [Fact]
+    public void RowsHaveValueForColumn_AllRowsHaveValue_ReturnsTrue()
+    {
+        bool valid = StructureDataValidator.RowsHaveValueForColumn(pointShapefile, "fd_id", out List<int> rowsWithMissingData);
+        Assert.True(valid);
+        Assert.True(rowsWithMissingData.Count == 0);
+
+    }
+
+    [Fact]
+    public void RowsHaveValueForColumn_MissingValues_ReturnsFalse()
+    {
+        bool valid = StructureDataValidator.RowsHaveValueForColumn(pointShapefile, "teachers", out List<int> rowsWithMissingData);
+        Assert.False(valid);
+        Assert.True(rowsWithMissingData.Count == 1);
+    }
+
+    [Fact]
+    public void AllRowsHaveUniqueValueForColumn_UniqueValues_ReturnsTrue()
+    {
+        //Act
+        bool valid = StructureDataValidator.AllRowsHaveUniqueValueForColumn<int>(pointShapefile, "TARGET_FID", out List<int> rowsWithDupes);
+
+        //Assert
+        Assert.True(valid);
+        Assert.True(rowsWithDupes.Count == 0);
+    }
+
+    [Fact]
+    public void AllRowsHaveUniqueValueForColumn_DuplicateValues_ReturnsFalse()
+    {
+        //Act
+        bool valid = StructureDataValidator.AllRowsHaveUniqueValueForColumn<int>(pointShapefile, "num_story", out List<int> rowsWithDupes);
+
+        //Assert
+        Assert.False(valid);
+        Assert.True(rowsWithDupes.Count > 0);
+
+    }
+}
