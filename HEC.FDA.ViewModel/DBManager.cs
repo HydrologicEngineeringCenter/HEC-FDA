6c6690a6540c88340bb6d5e159c0b745c9983ace
Jack <jackmschonherr@gmail.com>
2024-10-10 12:50:34 -0700

    creating initial DBManager class

---
+++ HEC.FDA.ViewModel/DBManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HEC.FDA.ViewModel;
+public class DBManager
+{
+    private SQLiteConnection _dbConnection;
+
+    public void SetDatabaseConnection(string fileName, SQLiteConnectionStringBuilder connectionBuilder = null)
+    {
+        connectionBuilder.DataSource = fileName;
+        _dbConnection = new SQLiteConnection(connectionBuilder.ToString());
+    }
+}
