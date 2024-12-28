8ebb18e7ccde2cc674070e39b544a50e7ca4a6a2
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2024-12-27 17:30:04 -0800

    Minor Changes to investigate failing Risk test.

--- ScratchSpace/EntryPoints/Beam.cs
+++ ScratchSpace/EntryPoints/Beam.cs
@@ -1,10 +1,26 @@
-﻿using HEC.FDA.Model.Spatial;
+﻿using HEC.FDA.Model.paireddata;
+using HEC.FDA.Model.Spatial;
 using RasMapperLib;
 
 namespace ScratchSpace.EntryPoints;
 public static class Beam
 {
     public static void EntryPoint()
+    {
+        TestIntegrate();
+    }
+
+    private static void TestIntegrate()
+    {
+        double[] xs = [.000001, 0.25, 0.5, 0.75, .999999];
+        double[] ys = [0, 6000, 8600, 136000, 500000];
+        PairedData pairedData = new(xs, ys);
+        double integral = pairedData.integrate();
+        Console.WriteLine(integral);
+        Console.Read();
+    }
+
+    private static void Whatever()
     {
         string tiffile = @"C:\Temp\FDA Bugs\NewLondonOrleans\NewLondonOrleans\Hydraulic Data\FWOP\FWOP 0.002 AEP\WSE (Max).Northern_Gulf_of_Mexico_Topobathy_DEM_14.tif";
         string shapefile = @"C:\Temp\FDA Bugs\NewLondonOrleans\NewLondonOrleans\Structure Inventories\SELA Structures\SELA_Struc_Final.shp";
@@ -15,7 +31,7 @@ public static class Beam
         int i = 8968;
 
         Console.WriteLine($"WSEs: {wses.Length}");
-            Console.WriteLine($"Structure {i+1}| WSE: {wses[i]} X: {pts[i].X} Y: {pts[i].Y}");
+        Console.WriteLine($"Structure {i + 1}| WSE: {wses[i]} X: {pts[i].X} Y: {pts[i].Y}");
         Console.Read();
     }
 }
