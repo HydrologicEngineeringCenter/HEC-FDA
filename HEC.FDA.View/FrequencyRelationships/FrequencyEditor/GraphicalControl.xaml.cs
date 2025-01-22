d7042e2eba895d15d6f13bb6e3df9cd014dc1cef
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2025-01-21 16:07:44 -0800

    fixes issue with radio buttons not loading on initial open, as well as malfunctioning when multiple tabs open

--- HEC.FDA.View/FrequencyRelationships/FrequencyEditor/GraphicalControl.xaml.cs
+++ HEC.FDA.View/FrequencyRelationships/FrequencyEditor/GraphicalControl.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System.Windows.Controls;
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
 
 namespace HEC.FDA.View.FrequencyRelationships.FrequencyEditor
 {
@@ -7,6 +9,16 @@ namespace HEC.FDA.View.FrequencyRelationships.FrequencyEditor
         public GraphicalControl()
         {
             InitializeComponent();
+            InstanceID = Guid.NewGuid().ToString();
         }
+
+        public string InstanceID
+        {
+            get { return (string)GetValue(InstanceIDProperty); }
+            set { SetValue(InstanceIDProperty, value); }
+        }
+
+        public static readonly DependencyProperty InstanceIDProperty =
+            DependencyProperty.Register("InstanceID", typeof(string), typeof(GraphicalControl), new PropertyMetadata(string.Empty));
     }
 }
