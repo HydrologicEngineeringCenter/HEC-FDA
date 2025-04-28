ba60b1ab35555f07eb744bcd1d567d793e7416f7
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2025-04-28 14:54:56 -0700

    fix column headers

--- HEC.FDA.View/FrequencyRelationships/FrequencyEditor/GraphicalControl.xaml.cs
+++ HEC.FDA.View/FrequencyRelationships/FrequencyEditor/GraphicalControl.xaml.cs
@@ -9,5 +9,7 @@ public partial class GraphicalControl : UserControl
     public GraphicalControl()
     {
         InitializeComponent();
+        _userEntryDataGrid.ColumnNameMappings.Add("X", "Exc. Prob");
+        _userEntryDataGrid.ColumnNameMappings.Add("Value", "Median");
     }
 }
