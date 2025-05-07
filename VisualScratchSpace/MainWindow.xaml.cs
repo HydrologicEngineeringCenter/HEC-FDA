09dde97ab12421c49c6dc81c1da7723a85044c25
Jack <jackmschonherr@gmail.com>
2025-05-07 11:29:15 -0700

    new VisualScratchSpace project

---
+++ VisualScratchSpace/MainWindow.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace VisualScratchSpace;
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    bool running = false;
+    public MainWindow()
+    {
+        InitializeComponent();
+
+    }
+
+    private void btnToggle_Click(object sender, RoutedEventArgs e)
+    {
+        running = !running;
+        btnToggle.Content = running ? "Stop" : "Run";
+        tbStatus.Text = running ? "Running" : "Stopped";
+    }
+}
