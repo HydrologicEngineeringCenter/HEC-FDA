4da62bcfc4ec9ab4be3f6c69277f6f52a2a434a7
Jack <jackmschonherr@gmail.com>
2025-05-22 12:13:35 -0700

    database interaction with dynamic combo boxes

--- VisualScratchSpace/Model/LifeLossDB.cs
+++ VisualScratchSpace/Model/LifeLossDB.cs
@@ -13,7 +13,37 @@ namespace VisualScratchSpace.Model
 
         public List<Simulation> UpdateSimulations()
         {
-            return null;
+            string query = "SELECT Name, Alternatives FROM Simulations_Lookup_Table";
+            List<Simulation> simulations = new List<Simulation>();
+
+            try
+            {
+                using SQLiteConnection connection = new SQLiteConnection(_connectionString);
+                connection.Open();
+                using var command = new SQLiteCommand(query, connection);
+                using var reader = command.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    Simulation simulation = new Simulation();
+
+                    // Name is 0th selection
+                    // Alternatives are 1st selection
+                    simulation.Name = reader.GetString(0);
+                    simulation.Alternatives = reader.GetString(1).Split(','); // Alternatives are in csv string format                   
+
+                    simulations.Add(simulation);
+                }
+            }
+            catch (Exception ex)
+            {
+                // debugging
+                Simulation simulation = new Simulation();
+                simulation.Name = ex.Message;
+                simulations.Add(simulation);
+            }
+            return simulations;
         }
+
     }
 }
