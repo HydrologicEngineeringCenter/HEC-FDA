ff8627c400f5d8b4aa2645b8d218357148cb43c9
Jack <jackmschonherr@gmail.com>
2025-10-31 13:43:30 -0700

    you can type decimals now

---
+++ HEC.FDA.View/Utilities/SafeDoubleConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace HEC.FDA.View.Utilities;
+public class SafeDoubleConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return value?.ToString() ?? "";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        var s = (value as string) ?? "";
+
+        // Keep typing for empty or “unfinished” inputs
+        var dec = culture.NumberFormat.NumberDecimalSeparator;
+
+        if (s.Length == 0 || s == "-" || s == dec || s.EndsWith(dec)) // ← this is the key: “12.” is in-progress
+        {
+            return Binding.DoNothing;
+        }
+
+        if (double.TryParse(s, NumberStyles.Float, culture, out var d))
+            return d;
+
+        return Binding.DoNothing; // invalid text: don’t update source yet
+    }
+}
