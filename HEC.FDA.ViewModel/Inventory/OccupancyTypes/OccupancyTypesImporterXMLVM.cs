e287babca2a8c29397bea38a48811e3c4162178e
Jack <jackmschonherr@gmail.com>
2025-11-06 16:31:38 -0800

    xml importing/exporting basic workflow in place

--- HEC.FDA.ViewModel/Inventory/OccupancyTypes/OccupancyTypesImporterXMLVM.cs
+++ HEC.FDA.ViewModel/Inventory/OccupancyTypes/OccupancyTypesImporterXMLVM.cs
@@ -1,14 +1,49 @@
-﻿using HEC.FDA.ViewModel.Editors;
+﻿using CommunityToolkit.Mvvm.Input;
+using HEC.FDA.ViewModel.Editors;
+using SciChart.Core.Extensions;
+using System.Xml.Linq;
 
 namespace HEC.FDA.ViewModel.Inventory.OccupancyTypes;
-public class OccupancyTypesImporterXMLVM : BaseEditorVM
+public partial class OccupancyTypesImporterXMLVM : BaseEditorVM
 {
+    private string _path;
+
+    public string Path
+    {
+        get { return _path; }
+        set { _path = value; NotifyPropertyChanged(); }
+    }
+
     public OccupancyTypesImporterXMLVM(EditorActionManager actionManager) : base(actionManager)
     {
 
     }
+
     public override void Save()
     {
-        throw new System.NotImplementedException();
+        // this will only be called when creating a new element (importing)
+        XElement occtypesRoot = XElement.Load(Path);
+        // have to find a new valid ID becaue this XML file could have come from any FDA study, IDs are scoped to studies
+        int id = Saving.PersistenceFactory.GetElementManager<OccupancyTypesElement>().GetNextAvailableId();
+
+        XElement header = occtypesRoot.Element("Header");
+        if (header == null)
+            throw new System.FormatException();
+
+        header.SetAttributeValue("ID", id);
+        // prepend the base name the user gives to the original occtypes name
+        string originalName = (string)header.Attribute("Name") ?? string.Empty;
+        header.SetAttributeValue("Name", $"{Name} {originalName}");
+        OccupancyTypesElement occtypes = new(occtypesRoot, id);
+        Save(occtypes);
+    }
+
+    [RelayCommand]
+    public void Import()
+    {
+        if (Path.IsNullOrEmpty())
+            return;
+
+        Save();
     }
 }
