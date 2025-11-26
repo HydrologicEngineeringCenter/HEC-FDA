bcfb638c46d3b7c265cdba109ac50f915a61e090
Jack Schonherr <jackmschonherr@gmail.com>
2025-11-26 09:48:30 -0800

    UI enhancements (#1277)
    Adds blue focus highlighting to study tree elements. Fixes #1272

--- HEC.FDA.View/Utilities/Converters/NullImageConverter.cs
+++ HEC.FDA.View/Utilities/Converters/NullImageConverter.cs
@@ -1,11 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Data;
 using System.Windows;
+using System.Windows.Data;
 
 namespace HEC.FDA.View.Utilities.Converters;
 public class NullImageConverter : IValueConverter
@@ -15,6 +11,36 @@ public class NullImageConverter : IValueConverter
         if (string.IsNullOrEmpty(value as string))
             return DependencyProperty.UnsetValue;
         return value;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
+
+public class NullImageToCollapsed : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty(value as string))
+            return Visibility.Collapsed;
+        return Visibility.Visible;
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        return Binding.DoNothing;
+    }
+}
+
+public class NullImageToVisible : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+    {
+        if (string.IsNullOrEmpty(value as string))
+            return Visibility.Visible;
+        return Visibility.Collapsed;
     }
 
     public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
