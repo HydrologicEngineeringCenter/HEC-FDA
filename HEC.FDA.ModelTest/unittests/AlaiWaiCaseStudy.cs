b5038e6b2edfae307c6888c194e22bd06d509113
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2026-02-04 13:21:30 -0800

    Add tests and data to AlaiWai

--- HEC.FDA.ModelTest/unittests/AlaiWaiCaseStudy.cs
+++ HEC.FDA.ModelTest/unittests/AlaiWaiCaseStudy.cs
@@ -35,6 +35,7 @@ public class AlaiWaiCaseStudy
             0.5
     };
 
+    // TimeOfDay = 2 (2am)
     double[] Reach1MeanLL = new double[]
     {
             2.186,
@@ -44,6 +45,91 @@ public class AlaiWaiCaseStudy
             0.017,
             0.009,
             0.003,
+            0
+    };
+
+    double[] Reach2MeanLL_2 = new double[]
+    {
+            0.037,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+
+    double[] Reach3MeanLL_2 = new double[]
+    {
+            0.003,
+            0.003,
+            0,
+            0,
+            0.001,
+            0,
+            0,
+            0
+    };
+
+    double[] Reach4MeanLL_2 = new double[]
+    {
+            0.003,
+            0,
+            0.003,
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+
+    // TimeOfDay = 14 (2pm)
+    double[] Reach1MeanLL_14 = new double[]
+    {
+            0.887,
+            0.159,
+            0.098,
+            0.079,
+            0.033,
+            0.014,
+            0.005,
+            0
+    };
+
+    double[] Reach2MeanLL_14 = new double[]
+    {
+            0.036,
+            0.006,
+            0.002,
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+
+    double[] Reach3MeanLL_14 = new double[]
+    {
+            0.01,
+            0.004,
+            0.003,
+            0,
+            0,
+            0,
+            0,
+            0
+    };
+
+    double[] Reach4MeanLL_14 = new double[]
+    {
+            0.003,
+            0.001,
+            0,
+            0,
+            0,
+            0,
+            0,
             0
     };
 
@@ -58,6 +144,42 @@ public class AlaiWaiCaseStudy
         PairedData reach1MeanLLCurve = new PairedData(NonExceedenceProbabilities, Reach1MeanLL);
         double calculatedAnnualLifeLoss = reach1MeanLLCurve.Integrate(asCDF);
         Assert.Equal(expectedAALL, calculatedAnnualLifeLoss, 4, MidpointRounding.ToEven);
+    }
+
+    [Theory]
+    [InlineData(2, 1, 0.0117965)]
+    [InlineData(2, 2, 0.0001295)]
+    [InlineData(2, 3, 0.0000625)]
+    [InlineData(2, 4, 0.000033)]
+    [InlineData(14, 1, 0.0094255)]
+    [InlineData(14, 2, 0.000165)]
+    [InlineData(14, 3, 0.0000735)]
+    [InlineData(14, 4, 0.0000145)]
+    public void AlaiWaiAllReachesMeanLifeLossIntegrationTest(int timeOfDay, int reach, double expectedAALL)
+    {
+        double[] reachMeanLL = GetReachMeanLL(timeOfDay, reach);
+        double[] nonExceedenceProbsCopy = (double[])NonExceedenceProbabilities.Clone();
+        double[] reachMeanLLCopy = (double[])reachMeanLL.Clone();
+        Array.Sort(nonExceedenceProbsCopy, reachMeanLLCopy);
+        PairedData reachMeanLLCurve = new PairedData(nonExceedenceProbsCopy, reachMeanLLCopy);
+        double calculatedAnnualLifeLoss = reachMeanLLCurve.Integrate(true); //testing with paddding.
+        Assert.Equal(expectedAALL, calculatedAnnualLifeLoss, 4, MidpointRounding.ToEven);
+    }
+
+    private double[] GetReachMeanLL(int timeOfDay, int reach)
+    {
+        return (timeOfDay, reach) switch
+        {
+            (2, 1) => Reach1MeanLL,
+            (2, 2) => Reach2MeanLL_2,
+            (2, 3) => Reach3MeanLL_2,
+            (2, 4) => Reach4MeanLL_2,
+            (14, 1) => Reach1MeanLL_14,
+            (14, 2) => Reach2MeanLL_14,
+            (14, 3) => Reach3MeanLL_14,
+            (14, 4) => Reach4MeanLL_14,
+            _ => throw new ArgumentException($"Invalid timeOfDay/reach combination: {timeOfDay}/{reach}")
+        };
     }
 
 
