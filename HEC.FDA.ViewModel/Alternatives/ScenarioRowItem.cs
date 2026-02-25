9fb99647a11f3fe5c63d4a084ad6d4d4d9403a28
Brennan Beam <64556723+Brennan1994@users.noreply.github.com>
2026-02-25 12:08:52 -0800

    Adds info to new alternative editor. Reverts null scenarios back to ""

--- HEC.FDA.ViewModel/Alternatives/ScenarioRowItem.cs
+++ HEC.FDA.ViewModel/Alternatives/ScenarioRowItem.cs
@@ -4,7 +4,7 @@ namespace HEC.FDA.ViewModel.Alternatives
 {
     public class ScenarioRowItem
     {
-        public const string NONE = "None";
+        public const string NONE = "";
         public IASElement Element { get; set; }
         public string Name => Element?.Name ?? NONE;
 
